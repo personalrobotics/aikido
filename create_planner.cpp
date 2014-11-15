4fea34064202c42bbd84bd5ed947e4002870ee68
Pras Velagapudi <psigen@gmail.com>
2014-11-15 18:26:01 -0500

    Update create_planner.cpp

--- create_planner.cpp
+++ create_planner.cpp
@@ -2,8 +2,18 @@
 
 class CustomPlanner : public r3::Planner 
 {
-  // Do some boost::python things here
-
+  // Let's define a custom property.
+  public int count = 0;
+  
+  // Let's define a custom member function.
+  public int increment(int x) {
+    count += x;
+    return count;
+  }
 }
+
+class_<CustomPlanner>("CustomPlanner")
+        .def("increment", &CustomPlanner::increment)
+        ;
 
 // What else do we need to do?
