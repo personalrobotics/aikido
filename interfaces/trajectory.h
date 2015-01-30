b24dffbb19938cf6cab2a5e2dafec4cf2cdeedaa
Pras Velagapudi <psigen@gmail.com>
2015-01-29 22:09:28 -0500

    Update trajectory.h

--- interfaces/trajectory.h
+++ interfaces/trajectory.h
@@ -1,30 +1,41 @@
-class State
+#ifndef _R3_TRAJECTORY_H_
+#define _R3_TRAJECTORY_H_
+
+#include <r3.h>
+
+namespace r3 
 {
-  /** The dimension of the state space */
-  int dim;
+  class State
+  {
+    /** The dimension of the state space */
+    int dim;
   
-  /** The order (number of derivatives) specified by the state */
-  int order;
+    /** The order (number of derivatives) specified by the state */
+    int order;
   
-  /** A 2D matrix of [order][dim], where each row is the n-th order state */
-  double[][] values;
+    /** A 2D matrix of [order][dim], where each row is the n-th order state */
+    double[][] values;
+  }
+
+  /**
+   * A path is an untimed list of states that a system will pass through.
+   */
+  class Path
+  {
+    std::vector<State> waypoints;
+  }
+
+  /**
+   * A trajectory is a function that maps from time to system state.
+   * Trajectories can have any internal representation. The only requirement is
+   * that they can be sampled at any time over their duration to return a State.
+   */
+  class Trajectory
+  {
+    State Sample(double time);
+    void Duration();
+  }
 }
 
-/**
- * A path is an untimed list of states that a system will pass through.
- */
-class Path
-{
-  std::vector<State> waypoints;
-}
 
-/**
- * A trajectory is a function that maps from time to system state.
- * Trajectories can have any internal representation. The only requirement is
- * that they can be sampled at any time over their duration to return a State.
- */
-class Trajectory
-{
-  State Sample(double time);
-  void Duration();
-}
+#endif // _R3_TRAJECTORY_H_
