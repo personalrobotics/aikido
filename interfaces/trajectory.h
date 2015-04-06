1eb31a2cffd9011d08b8200d6dc81f12de5957b8
Pras Velagapudi <psigen@gmail.com>
2015-04-06 13:12:10 -0400

    Update trajectory.h

--- interfaces/trajectory.h
+++ interfaces/trajectory.h
@@ -11,19 +11,33 @@ namespace r3
   class Path
   {
     std::vector<State> waypoints;
-  }
+  };
 
   /**
    * A trajectory is a function that maps from time to system state.
    * Trajectories can have any internal representation. The only requirement is
    * that they can be sampled at any time over their duration to return a State.
    */
+  template <typename State>
   class Trajectory
   {
     State Sample(double time);
     void Duration();
-  }
-}
+    unsigned int Order();
+  };
+  
+  class RealTrajectory
+  : Trajectory<double>
+  {};
 
+  class QuaternionTrajectory
+  : Trajectory<Quaternion>
+  {};
+  
+  template <typename State>
+  class DOFTrajectory
+  : std::vector<State>
+  {};
+}
 
 #endif // _R3_TRAJECTORY_H_
