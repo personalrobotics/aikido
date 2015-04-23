4b6dfe70602c85b785536ff48f00ef04ea347ba4
Michael Koval <mkoval@cs.cmu.edu>
2015-04-22 21:23:04 -0400

    stuff

--- scripts/load_herb.py
+++ scripts/load_herb.py
@@ -12,15 +12,17 @@ URDF_PATH \
 PACKAGE_NAME = 'ada_description'
 PACKAGE_PATH = '/home/parallels/ros-ada/src/ada/ada_description'
 URDF_PATH = '/home/parallels/ros-ada/src/ada/ada_description/robots/mico.urdf'
+OBJECT_PATH = '../urdf/fuze_bottle.urdf'
 DOF_NAMES = [ 'j1', 'j2', 'j3', 'j4', 'j5', 'j6' ]
 
 urdf_loader = r3py.DartLoader()
 urdf_loader.add_package_directory(PACKAGE_NAME, PACKAGE_PATH)
 robot = urdf_loader.parse_skeleton(URDF_PATH)
+obj = urdf_loader.parse_skeleton(OBJECT_PATH)
 
 world = r3py.World()
-robot_name = world.add_skeleton(robot)
-robot = world.get_skeleton_by_name(robot_name)
+robot = world.get_skeleton_by_name(world.add_skeleton(robot))
+obj = world.get_skeleton_by_name(world.add_skeleton(obj))
 
 dofs = [ robot.get_dof_by_name(name) for name in DOF_NAMES ]
 weights = numpy.ones(6)
@@ -34,9 +36,8 @@ window.world = world
 
 path = r3py.ompl_plan(world, dofs, weights, resolutions, q_start, q_goal)
 
+"""
 for waypoint1, waypoint2 in zip(path[:-1], path[1:]):
-    print 'Waypoint!'
-
     for r in numpy.linspace(0., 1., 200):
         values = (1 - r) * waypoint1 + (r) * waypoint2
 
@@ -44,3 +45,4 @@ for waypoint1, waypoint2 in zip(path[:-1], path[1:]):
             dof.position = value
 
         time.sleep(0.02)
+"""
--- src/python/python_Skeleton.cpp
+++ src/python/python_Skeleton.cpp
@@ -2,11 +2,21 @@
 #include <dart/dynamics/dynamics.h>
 #include "util.h"
 
+using ::dart::dynamics::Skeleton;
+
+static Eigen::Isometry3d Skeleton_getPose(Skeleton *skeleton)
+{
+    //FreeJoint::convertToTransform(
+}
+
 void python_Skeleton()
 {
     using namespace ::boost::python;
+    using ::dart::dynamics::BodyNode;
     using ::dart::dynamics::DegreeOfFreedom;
-    using ::dart::dynamics::Skeleton;
+    using ::dart::dynamics::Joint;
+    using ::dart::dynamics::Marker;
+    using ::dart::dynamics::SoftBodyNode;
     using ::r3::python::util::collection_from_python;
 
     collection_from_python<std::vector<Skeleton *> >();
@@ -20,7 +30,38 @@ void python_Skeleton()
             &Skeleton::isEnabledSelfCollisionCheck)
         .add_property("is_enabled_adjacent_body_check",
             &Skeleton::isEnabledAdjacentBodyCheck)
-        .add_property("position", &Skeleton::setPosition, &Skeleton::getPosition)
+        .add_property("position", &Skeleton::getPositions, &Skeleton::setPositions)
+        .add_property("is_mobile", &Skeleton::isMobile, &Skeleton::setMobile)
+        .add_property("timestep", &Skeleton::getTimeStep, &Skeleton::setTimeStep)
+        .add_property("gravity",
+            make_function(&Skeleton::getGravity,
+                          return_value_policy<copy_const_reference>()),
+            &Skeleton::setGravity)
+        .add_property("mass", &Skeleton::getMass)
+        .add_property("num_body_nodes", &Skeleton::getNumBodyNodes)
+        .add_property("num_rigid_body_nodes", &Skeleton::getNumRigidBodyNodes)
+        .add_property("num_soft_body_nodes", &Skeleton::getNumSoftBodyNodes)
+        .add_property("root_body_node",
+            make_function(
+                static_cast<BodyNode *(Skeleton::*)()>(
+                    &Skeleton::getRootBodyNode),
+                return_value_policy<reference_existing_object>()))
+        .add_property("num_dofs", &Skeleton::getNumDofs)
+        .def("get_marker_by_name",
+            make_function(
+                static_cast<Marker *(Skeleton::*)(std::string const &)>(
+                    &Skeleton::getMarker),
+                return_value_policy<reference_existing_object>()))
+        .def("get_joint",
+            make_function(
+                static_cast<Joint *(Skeleton::*)(size_t)>(
+                    &Skeleton::getJoint),
+                return_value_policy<reference_existing_object>()))
+        .def("get_joint_by_name",
+            make_function(
+                static_cast<Joint *(Skeleton::*)(std::string const &)>(
+                    &Skeleton::getJoint),
+                return_value_policy<reference_existing_object>()))
         .def("get_dof",
             make_function(
                 static_cast<DegreeOfFreedom *(Skeleton::*)(size_t)>(
@@ -31,8 +72,30 @@ void python_Skeleton()
                 static_cast<DegreeOfFreedom *(Skeleton::*)(std::string const &)>(
                     &Skeleton::getDof),
                 return_value_policy<reference_existing_object>()))
+        .def("get_body_node",
+            make_function(
+                static_cast<BodyNode *(Skeleton::*)(size_t)>(
+                    &Skeleton::getBodyNode),
+                return_value_policy<reference_existing_object>()))
+        .def("get_body_node_by_name",
+            make_function(
+                static_cast<BodyNode *(Skeleton::*)(std::string const &)>(
+                    &Skeleton::getBodyNode),
+                return_value_policy<reference_existing_object>()))
+        .def("get_soft_body_node",
+            make_function(
+                static_cast<BodyNode *(Skeleton::*)(size_t)>(
+                    &Skeleton::getBodyNode),
+                return_value_policy<reference_existing_object>()))
+        .def("get_soft_body_node_by_name",
+            make_function(
+                static_cast<SoftBodyNode *(Skeleton::*)(std::string const &)>(
+                    &Skeleton::getSoftBodyNode),
+                return_value_policy<reference_existing_object>()))
         .def("enable_self_collision", &Skeleton::enableSelfCollision)
         .def("disable_self_collision", &Skeleton::disableSelfCollision)
         .def("compute_forward_kinematics", &Skeleton::computeForwardKinematics)
+        .def("add_body_node", &Skeleton::addBodyNode)
+        .def("init", &Skeleton::init)
         ;
 }
