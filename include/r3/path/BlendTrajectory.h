3ceae5cb5867874c7d3b8f18452839cfc4176645
Michael Koval <michael.c.koval@nasa.gov>
2015-07-21 16:41:00 -0500

    Added a BlendTrajectory class.

---
+++ include/r3/path/BlendTrajectory.h
@@ -0,0 +1,34 @@
+#ifndef R3_PATH_BLENDTRAJECTORY_H_
+#define R3_PATH_BLENDTRAJECTORY_H_
+#include <r3/path/Spline.h>
+
+namespace r3 {
+namespace path {
+
+class BlendTrajectory : public virtual Trajectory {
+public:
+  BlendTrajectory(
+    const ConstTrajectoryPtr& _traj1,
+    const ConstTrajectoryPtr& _traj2,
+    double _timeStart,
+    double _TimeEnd);
+
+  Index getNumOutputs() const override;
+
+  Index getNumDerivatives() const override;
+
+  Scalar getDuration() const override;
+
+  Eigen::VectorXd evaluate(Scalar _t, Index _derivative) const override;
+
+private:
+  ConstTrajectoryPtr mTrajectory1;
+  ConstTrajectoryPtr mTrajectory2;
+  double mTimeStart;
+  double mTimeEnd;
+};
+
+} // namespace path
+} // namespace r3
+
+#endif // ifndef R3_PATH_BLENDTRAJECTORY_H_
---
+++ src/path/BlendTrajectory.cpp
@@ -0,0 +1,56 @@
+#include <r3/path/BlendTrajectory.h>
+
+namespace r3 {
+namespace path {
+
+BlendTrajectory::BlendTrajectory(
+      const ConstTrajectoryPtr& _traj1,
+      const ConstTrajectoryPtr& _traj2,
+      double _timeStart,
+      double _timeEnd)
+  : mTrajectory1(_traj1),
+    mTrajectory2(_traj2),
+    mTimeStart(_timeStart),
+    mTimeEnd(
+      std::min(
+        _timeEnd, std::min(
+        _traj1->getDuration(),
+        _traj2->getDuration())))
+{
+  assert(_timeStart <= _timeEnd);
+  assert(_traj1->getNumOutputs() == _traj2->getNumOutputs());
+}
+
+auto BlendTrajectory::getNumOutputs() const -> Index
+{
+  return mTrajectory1->getNumOutputs();
+}
+
+auto BlendTrajectory::getNumDerivatives() const -> Index
+{
+  return std::max(
+    mTrajectory1->getNumDerivatives(),
+    mTrajectory2->getNumDerivatives());
+}
+
+auto BlendTrajectory::getDuration() const -> Scalar
+{
+  return mTrajectory1->getDuration();
+}
+
+Eigen::VectorXd BlendTrajectory::evaluate(Scalar _t, Index _derivative) const
+{
+  if (_t <= mTimeStart)
+    return mTrajectory1->evaluate(_t, _derivative);
+  else if (_t >= mTimeEnd)
+    return mTrajectory2->evaluate(_t, _derivative);
+  else
+  {
+    const double r = (_t - mTimeStart) / (mTimeEnd - mTimeStart);
+    return (1 - r) * mTrajectory1->evaluate(_t, _derivative)
+         + (    r) * mTrajectory2->evaluate(_t, _derivative);
+  }
+}
+
+} // namespace path
+} // namespace r3
