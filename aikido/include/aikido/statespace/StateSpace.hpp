99791c154e75b62bb51566d3367e8b4358be51f9
Michael Koval <mkoval@cs.cmu.edu>
2016-04-04 01:39:34 -0400

    Added comments to base StateSpace class.

--- aikido/include/aikido/statespace/StateSpace.hpp
+++ aikido/include/aikido/statespace/StateSpace.hpp
@@ -5,20 +5,29 @@
 namespace aikido {
 namespace statespace {
 
+/// Base class for all StateSpaces.
 class StateSpace {
 public:
-  class State {
+  /// Base class for all States.
+  class State
+  {
   public:
+    // TODO: Remove this virtual destructor. Having any virtual function in
+    // this class, including this destructor, adds sizeof(pointer) overhead.
     virtual ~State() = default;
+
+  protected:
+    State() = default;
   };
 
   virtual ~StateSpace() = default;
 
+  /// TODO: Need a docstring for this.
   virtual void compose(const State& _state1, const State& _state2,
                        State& _out) const = 0;
 };
 
-using StateSpacePtr = std::shared_ptr<const StateSpace>;
+using StateSpacePtr = std::shared_ptr<StateSpace>;
 
 } // namespace statespace
 } // namespace aikido
