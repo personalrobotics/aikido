94c71d74c2ec7ca1d32f09deb01e22d0b395cd56
Clint Liddick <clint.liddick@gmail.com>
2016-04-07 07:09:23 -0400

    fix incorrect comment
    never commit comments after midnight

--- aikido/src/util/VanDerCorput.cpp
+++ aikido/src/util/VanDerCorput.cpp
@@ -44,16 +44,16 @@ VanDerCorput::DTuple VanDerCorput::compute_vandercorput(int n) const
   double ret = 0.0;
 
   // Treat Van Der Corput sequence like a binary tree.
-  // The each node that finishes a perfect tree
+  // Each node that completes a perfect tree
   // reduces the resolution by cutting the final remaining
   // segment of the last resolution size.
   // So to find the resolution...
   double power = std::ceil(std::log2(n + 1)) - 1;  // calc height of tree
   double next_power =
       std::ceil(std::log2(n + 2)) - 1;  // and height after next node is added.
-  if (power == next_power) {  // If next node does not finish perfect tree
-    resolution = 1. / (std::pow(2, power));  // height is resolution
-  } else {  // if next node does complete perfect tree
+  if (power == next_power) {            // If next node does not start new level
+    resolution = 1. / (std::pow(2, power));  // not yet perfect tree
+  } else {  // if next node does start new level
     resolution = 1. / (std::pow(2, power + 1));  // shrink resolution
   }
 
