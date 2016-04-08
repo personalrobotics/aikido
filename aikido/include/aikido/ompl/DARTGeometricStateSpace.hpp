5132bbdd1c207da255a651c1fd2b041b6c7c8ef7
Jennifer King <jeking@andrew.cmu.edu>
2016-04-08 06:13:25 -0700

    OMPL state space building

--- aikido/include/aikido/ompl/DARTGeometricStateSpace.hpp
+++ aikido/include/aikido/ompl/DARTGeometricStateSpace.hpp
@@ -1,8 +1,11 @@
 #ifndef DART_GEOMETRIC_STATE_SPACE_H
 #define DART_GEOMETRIC_STATE_SPACE_H
 
+#include <ompl/base/StateSpace.h>
+#include <aikido/statespace/StateSpace.hpp>
+
 namespace aikido {
-    namespace ompl {
+    namespace ompl_bindings {
 
         // Basic idea: Wrap the state space and associated state
         //  into a state space recognizable by OMPL
@@ -12,73 +15,46 @@ namespace aikido {
 
         public:
 
-            class StateType : public State {
+            class StateType : public ompl::base::State {
             public:
-                StateType(aikido::statespace::StateSpace::State* _st) : st(_st) {}
+                StateType(aikido::statespace::StateSpace::State* _st) : mState(_st) {}
 
                 aikido::statespace::StateSpace::State* mState;
             };
             
-            DARTGeometricStateSpace(const aikido::statespace::StateSpacePtr &_sspace)
-                : mStateSpace(_sspace) {
-            }
+            DARTGeometricStateSpace(const aikido::statespace::StateSpacePtr &_sspace);
 
             /// Get the dimension of the space (not the dimension of the surrounding ambient space)
-            virtual unsigned int getDimension() const {
-                return mStateSpace->getDimension();
-            }
+            virtual unsigned int getDimension() const;
 
             /// Get the maximum value a call to distance() can return (or an upper bound). 
             /// For unbounded state spaces, this function can return infinity.
-            virtual double getMaximumExtent() const {
-                return mStateSpace->getMaximumExtent();
-            }
+            virtual double getMaximumExtent() const;
 
             /// Get a measure of the space (this can be thought of as a generalization of volume) 
-            virtual double getMeasure() const {
-                return mStateSpace->getMeasure();
-            }
+            virtual double getMeasure() const;
 
             /// Bring the state within the bounds of the state space. 
             /// For unbounded spaces this function can be a no-op.
-            virtual void enforceBounds(ompl::base::State* _state) const {
-                StateType* st = static_cast<StateType*>(_state);
-                mStateSpace->enforceBounds(st->mState);
-            }
+            virtual void enforceBounds(ompl::base::State* _state) const;
 
             /// Check if a state is inside the bounding box. 
             /// For unbounded spaces this function can always return true.
-            virtual bool satisfiesBounds(const ompl::base::State* _state) const {
-                StateType* st = static_cast<StateType*>(_state);
-                return mStateSpace->satisfiesBounds(st->mState);
-            }
+            virtual bool satisfiesBounds(const ompl::base::State* _state) const;
 
             /// Copy a state to another.
             virtual void copyState(ompl::base::State* _destination, 
-                                   const ompl::base::State* _source) const {
-                
-                StateType* dst = static_cast<StateType*>(_destination);
-                StateType* sst = static_cast<StateType*>(_source);
-                mStateSpace->copyState(dst.mState, sst.mState);
-            }
+                                   const ompl::base::State* _source) const;
 
             /// Computes distance between two states. This function satisfies 
             /// the properties of a metric if isMetricSpace() is true, and its 
             /// return value will always be between 0 and getMaximumExtent()
             virtual double distance(const ompl::base::State* _state1,
-                                    const ompl::base::State* _state2) const {
-                StateType* st1 = static_cast<StateType*>(_state1);
-                StateType* st2 = static_cast<StateType*>(_state2);
-                return mStateSpace->distance(st1.mState, st2.mState);
-            }
+                                    const ompl::base::State* _state2) const;
             
             /// Check state equality
             virtual bool equalStates(const ompl::base::State* _state1,
-                                       const ompl::base::State* _state2) const {
-                StateType* st1 = static_cast<StateType*>(_state1);
-                StateType* st2 = static_cast<StateType*>(_state2);
-                return mStateSpace->equalStates(st1.mState, st2.mState);
-            }
+                                     const ompl::base::State* _state2) const;
 
             /// Computes the state that lies at time t in [0, 1] on the segment 
             /// that connects from state to to state. The memory location of state 
@@ -86,37 +62,19 @@ namespace aikido {
             virtual void interpolate(const ompl::base::State* _from,
                                      const ompl::base::State* _to,
                                      const double _t,
-                                     ompl::base::State* _state) const {
-                const StateType* fromState = static_cast<const StateType*>(_state1);
-                const StateType* toState = static_cast<const StateType*>(_state2);
-                StateType* iState = static_cast<StateType*>(_state);
-                
-                return mStateSpace->interpolate(fromState.mState,
-                                                toState.mState,
-                                                _t, iState.mState);
-            }
+                                     ompl::base::State* _state) const;
 
             /// Allocate an instance of the state sampler for this space. 
             /// This sampler will be allocated with the sampler allocator that 
             /// was previously specified by setStateSamplerAllocator() or, 
             /// if no sampler allocator was specified, allocDefaultStateSampler() is called.
-            virtual ompl::base::StateSamplerPtr allocDefaultStateSampler() const {
-                // TODO
-            }
+            virtual ompl::base::StateSamplerPtr allocDefaultStateSampler() const;
                 
             /// Allocate a state that can store a point in the described space
-            virtual ompl::base::State* allocState() const {
-                aikido::statespace::StateSpace::State* ast =
-                    mStateSpace->allocateState();
-                return new StateType(ast);
-            }
+            virtual ompl::base::State* allocState() const;
             
             /// Free the memory of the allocated state
-            virtual void freeState(ompl::base::State* _state) const {
-                StateType* st = static_cast<StateType*>(_state);
-                mStateSpace->freeState(st.st);
-                delete st;
-            }
+            virtual void freeState(ompl::base::State* _state) const;
 
         private:
             aikido::statespace::StateSpacePtr mStateSpace;
