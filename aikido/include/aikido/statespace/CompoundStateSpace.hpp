2c1adc775db970746a43ba04105e12bd2759cfc0
Clint Liddick <clint.liddick@gmail.com>
2016-04-08 16:37:53 -0400

    fixed shared_from on CompoundStateSpace

--- aikido/include/aikido/statespace/CompoundStateSpace.hpp
+++ aikido/include/aikido/statespace/CompoundStateSpace.hpp
@@ -12,7 +12,7 @@ template <class> class CompoundStateHandle;
 
 /// Represents the Cartesian product of other StateSpaces.
 class CompoundStateSpace
-  : std::enable_shared_from_this<CompoundStateSpace>
+  : public std::enable_shared_from_this<CompoundStateSpace>
   , public virtual StateSpace
 {
 public:
