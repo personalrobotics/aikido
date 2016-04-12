9ecea739003cd672a76628590c7422ba5cf204e3
Clint Liddick <clint.liddick@gmail.com>
2016-04-12 10:36:52 -0400

    add main Snap test cases; refactor to mock constraints

--- aikido/tests/test_SnapPlanner.cpp
+++ aikido/tests/test_SnapPlanner.cpp
@@ -1,22 +1,70 @@
 #include <gtest/gtest.h>
 #include <aikido/planner/PlanningResult.hpp>
 #include <aikido/planner/SnapPlanner.hpp>
+#include <aikido/statespace/SO2StateSpace.hpp>
 #include <aikido/statespace/dart/MetaSkeletonStateSpace.hpp>
 #include <aikido/constraint/CollisionConstraint.hpp>
+#include <aikido/constraint/TestableConstraint.hpp>
 #include <dart/dart.h>
 #include <tuple>
 
 using std::shared_ptr;
 using std::make_shared;
 
+class FailingConstraint : public aikido::constraint::TestableConstraint
+{
+public:
+  FailingConstraint(shared_ptr<aikido::statespace::StateSpace> stateSpace)
+      : stateSpace{stateSpace}
+  {
+  }
+
+  bool isSatisfied(
+      const aikido::statespace::StateSpace::State* state) const override
+  {
+    return false;
+  }
+
+  const std::shared_ptr<aikido::statespace::StateSpace> getStateSpace()
+      const override
+  {
+    return stateSpace;
+  }
+
+private:
+  shared_ptr<aikido::statespace::StateSpace> stateSpace;
+};
+
+class PassingConstraint : public aikido::constraint::TestableConstraint
+{
+public:
+  PassingConstraint(shared_ptr<aikido::statespace::StateSpace> stateSpace)
+      : stateSpace{stateSpace}
+  {
+  }
+
+  bool isSatisfied(
+      const aikido::statespace::StateSpace::State* state) const override
+  {
+    return true;
+  }
+
+  const std::shared_ptr<aikido::statespace::StateSpace> getStateSpace()
+      const override
+  {
+    return stateSpace;
+  }
+
+private:
+  shared_ptr<aikido::statespace::StateSpace> stateSpace;
+};
+
 class SnapPlannerTest : public ::testing::Test
 {
 public:
-  using FCLCollisionDetector = dart::collision::FCLCollisionDetector;
   using MetaSkeletonStateSpace = aikido::statespace::MetaSkeletonStateSpace;
-  using RealVectorStateSpace = aikido::statespace::RealVectorStateSpace;
+  using SO2StateSpace = aikido::statespace::SO2StateSpace;
   using ScopedState = MetaSkeletonStateSpace::ScopedState;
-  using CollisionConstraint = aikido::constraint::CollisionConstraint;
 
   using BodyNodePtr = dart::dynamics::BodyNodePtr;
   using JointPtr = dart::dynamics::JointPtr;
@@ -25,16 +73,11 @@ public:
   SnapPlannerTest()
       : skel{dart::dynamics::Skeleton::create("skel")}
       , jn_bn{skel->createJointAndBodyNodePair<dart::dynamics::RevoluteJoint>()}
-      , cd{FCLCollisionDetector::create()}
-    {
-      // set joint limit to make a RealVectorStateSpace
-      // instead of an SO2StateSpace
-      jn_bn.first->setPositionUpperLimit(0, 3);
-      stateSpace.reset(new MetaSkeletonStateSpace(skel));
-      startState.reset(new ScopedState(stateSpace->createState()));
-      goalState.reset(new ScopedState(stateSpace->createState()));
-      collConstraint.reset(new CollisionConstraint(stateSpace, cd));
-    };
+      , stateSpace{make_shared<MetaSkeletonStateSpace>(skel)}
+      , startState{make_shared<ScopedState>(stateSpace->createState())}
+      , goalState{make_shared<ScopedState>(stateSpace->createState())}
+      , passingConstraint{make_shared<PassingConstraint>(stateSpace)}
+      , failingConstraint{make_shared<FailingConstraint>(stateSpace)} {};
   // DART setup
   SkeletonPtr skel;
   std::pair<JointPtr, BodyNodePtr> jn_bn;
@@ -43,8 +86,8 @@ public:
   shared_ptr<MetaSkeletonStateSpace> stateSpace;
   shared_ptr<ScopedState> startState;
   shared_ptr<ScopedState> goalState;
-  shared_ptr<FCLCollisionDetector> cd;
-  shared_ptr<CollisionConstraint> collConstraint;
+  shared_ptr<PassingConstraint> passingConstraint;
+  shared_ptr<FailingConstraint> failingConstraint;
   aikido::planner::PlanningResult planningResult;
 };
 
@@ -53,28 +96,46 @@ TEST_F(SnapPlannerTest, ThrowsOnStateSpaceMismatch)
   SkeletonPtr empty_skel = dart::dynamics::Skeleton::create("skel");
   auto differentStateSpace = make_shared<MetaSkeletonStateSpace>(empty_skel);
   EXPECT_THROW(planSnap(*startState, *goalState, differentStateSpace,
-                        collConstraint, &planningResult),
+                        passingConstraint, &planningResult),
                std::invalid_argument);
 }
 
-TEST_F(SnapPlannerTest, ReturnsStartGoalTrajOnSuccess)
+TEST_F(SnapPlannerTest, ReturnsStartToGoalTrajOnSuccess)
 {
   stateSpace->getStateFromMetaSkeleton(*startState);
-  stateSpace->getMetaSkeleton()->setPosition(0, 10.0);
+  stateSpace->getMetaSkeleton()->setPosition(0, 2.0);
   stateSpace->getStateFromMetaSkeleton(*goalState);
 
-  auto traj = planSnap(*startState, *goalState, stateSpace, collConstraint,
+  auto traj = planSnap(*startState, *goalState, stateSpace, passingConstraint,
                        &planningResult);
 
-  auto subSpace = stateSpace->getSubSpace<RealVectorStateSpace>(0);
+  auto subSpace = stateSpace->getSubSpace<SO2StateSpace>(0);
   EXPECT_EQ(2, traj->size());
 
   auto startValue =
-      startState->getSubStateHandle<RealVectorStateSpace>(0).getValue();
+      startState->getSubStateHandle<SO2StateSpace>(0).getRotation();
 
-  auto traj0 = stateSpace->getSubStateHandle<RealVectorStateSpace>(
-    static_cast<MetaSkeletonStateSpace::State*>(traj->at(0).getState()), 0)
-      .getValue();
+  auto traj0 = stateSpace->getSubStateHandle<SO2StateSpace>(
+                               static_cast<MetaSkeletonStateSpace::State*>(
+                                   traj->at(0).getState()),
+                               0).getRotation();
 
   EXPECT_TRUE(startValue.isApprox(traj0));
+
+  auto goalValue = goalState->getSubStateHandle<SO2StateSpace>(0).getRotation();
+
+  auto traj1 = stateSpace->getSubStateHandle<SO2StateSpace>(
+                               static_cast<MetaSkeletonStateSpace::State*>(
+                                   traj->at(traj->size() - 1).getState()),
+                               0).getRotation();
+
+  EXPECT_TRUE(goalValue.isApprox(traj1))
+      << "on success final element of trajectory should be goal state.";
+}
+
+TEST_F(SnapPlannerTest, FailIfConstraintNotSatisfied)
+{
+  auto traj = planSnap(*startState, *goalState, stateSpace, failingConstraint,
+                       &planningResult);
+  EXPECT_EQ(nullptr, traj) << "Failed plan should return `nullptr`";
 }
