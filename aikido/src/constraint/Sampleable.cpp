dd43676904273bec8a514a10afaa368565966642
Michael Koval <mkoval@cs.cmu.edu>
2016-04-13 14:06:47 -0400

    Added definition of NO_LIMIT to enable odr-use.

---
+++ aikido/src/constraint/Sampleable.cpp
@@ -0,0 +1,9 @@
+#include <aikido/constraint/Sampleable.hpp>
+
+namespace aikido {
+namespace constraint {
+
+constexpr int SampleGenerator::NO_LIMIT;
+
+} // namespace constraint
+} // namespace aikido
