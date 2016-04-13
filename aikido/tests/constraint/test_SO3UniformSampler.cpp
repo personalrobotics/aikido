bb496b68bb397092a216c619a65e8fe0232964c1
Michael Koval <mkoval@cs.cmu.edu>
2016-04-13 16:13:25 -0400

    Moved test coverage helper to a separate file.

--- aikido/tests/constraint/test_SO3UniformSampler.cpp
+++ aikido/tests/constraint/test_SO3UniformSampler.cpp
@@ -2,6 +2,7 @@
 #include <dart/common/StlHelpers.h>
 #include <aikido/constraint/uniform/SO3UniformSampler.hpp>
 #include <aikido/distance/GeodesicDistanceMetric.hpp>
+#include "SampleGeneratorCoverage.hpp"
 
 using aikido::statespace::SO3StateSpace;
 using aikido::statespace::SO3UniformSampler;
@@ -50,44 +51,6 @@ protected:
   std::shared_ptr<GeodesicDistanceMetric> mDistance;
   std::vector<SO3StateSpace::ScopedState> mTargets;
 };
-
-
-template <class Iterator>
-testing::AssertionResult SampleGeneratorCoverage(
-  aikido::constraint::SampleGenerator& _generator,
-  const aikido::distance::DistanceMetric& _metric,
-  const Iterator _beginTargets, const Iterator _endTargets,
-  double _distanceThreshold, size_t _numSamples)
-{
-  std::vector<int> counts(std::distance(_beginTargets, _endTargets), 0);
-
-  const auto stateSpace = _generator.getStateSpace();
-  auto state = stateSpace->createState();
-
-  for (size_t isample = 0; isample < _numSamples; ++isample)
-  {
-    if (!_generator.canSample())
-      return ::testing::AssertionFailure() << "Unable to sample.";
-
-    if (!_generator.sample(state))
-      return ::testing::AssertionFailure() << "Failed sampling.";
-
-    size_t itarget = 0;
-    for (Iterator it = _beginTargets; it != _endTargets; ++it, ++itarget)
-    {
-      if (_metric.distance(state, *it) < _distanceThreshold)
-        counts[itarget]++;
-    }
-  }
-
-  for (auto count : counts)
-  {
-    if (count == 0)
-      return ::testing::AssertionFailure() << "Missed one or more targets.";
-  }
-  
-  return testing::AssertionSuccess();
-}
 
 TEST_F(SO3UniformSamplerTests, constructor_StateSpaceIsNull_Throws)
 {
