db56185328bfb305cc0e3aeb3a8ccbec6114c4ad
Michael Koval <mkoval@cs.cmu.edu>
2016-04-18 14:36:37 -0400

    Fixed HauserParabolicSmoother on OS X.

--- aikido/src/external/hauser_parabolic_smoother/HauserMath.h
+++ aikido/src/external/hauser_parabolic_smoother/HauserMath.h
@@ -1,8 +1,8 @@
 #ifndef PARABOLIC_RAMP_MATH_H
 #define PARABOLIC_RAMP_MATH_H
 
-#include <math.h>
-#include <stdlib.h>
+#include <cmath>
+#include <cstdlib>
 #include <vector>
 
 namespace ParabolicRamp {
@@ -13,11 +13,11 @@ typedef std::vector<double> Vector;
 //can replace this with your favorite representation/tests of infinity
 const static Real Inf = 1e300;
 inline bool IsInf(Real x) { return x==Inf; }
-inline bool IsFinite(Real x) { return fabs(x)<Inf; }
+inline bool IsFinite(Real x) { return std::abs(x)<Inf; }
 
 inline Real Sqr(Real x) { return x*x; }
-inline Real Sqrt(Real x) { return sqrt(x); }
-inline Real Abs(Real x) { return fabs(x); }
+inline Real Sqrt(Real x) { return std::sqrt(x); }
+inline Real Abs(Real x) { return std::abs(x); }
 inline Real Sign(Real x) { return (x>0 ? 1 : (x<0 ? -1 : 0)); }
 inline Real Min(Real x,Real y) { return (x<y?x:y); }
 inline Real Max(Real x,Real y) { return (x>y?x:y); }
