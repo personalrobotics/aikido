f59ea31035dbac8f2365b9dc16fbaed095ac77b1
Michael Koval <mkoval@cs.cmu.edu>
2016-04-21 16:26:14 -0400

    Moved SnapPlanner into a .cpp file.

--- aikido/include/aikido/planner/SnapPlanner.hpp
+++ aikido/include/aikido/planner/SnapPlanner.hpp
@@ -5,49 +5,19 @@
 #include <exception>
 #include <stdexcept>
 #include <vector>
-#include "PlanningResult.hpp"
-#include "../constraint/TestableConstraint.hpp"
-#include "../distance/DistanceMetric.hpp"
-#include "../path/PiecewiseLinearTrajectory.hpp"
-#include "../statespace/StateSpace.hpp"
-#include "../statespace/Interpolator.hpp"
-#include "../util/VanDerCorput.hpp"
 
-namespace aikido
-{
-namespace planner
-{
-static path::PiecewiseLinearTrajectoryPtr planSnap(
-    const aikido::statespace::StateSpace::State *startState,
-    const aikido::statespace::StateSpace::State *goalState,
-    const std::shared_ptr<aikido::statespace::StateSpace> stateSpace,
-    const std::shared_ptr<aikido::constraint::TestableConstraint> constraint,
-    const std::shared_ptr<aikido::statespace::Interpolator> interpolator,
-    aikido::planner::PlanningResult *planningResult)
-{
-  if (stateSpace != constraint->getStateSpace()) {
-    throw std::invalid_argument(
-        "StateSpace of constraint not equal to StateSpace of planning space");
-  }
-  aikido::util::VanDerCorput vdc{1, true, 0.02};  // TODO junk resolution
-  auto returnTraj = std::make_shared<aikido::path::PiecewiseLinearTrajectory>(
-      stateSpace, interpolator);
-  auto testState = stateSpace->createState();
+namespace aikido {
+namespace planner {
 
-  for (double alpha : vdc) {
-    interpolator->interpolate(startState, goalState, alpha, testState);
-    if (!constraint->isSatisfied(testState)) {
-      planningResult->message = "Collision detected";
-      return returnTraj;
-    }
-  }
+path::PiecewiseLinearTrajectoryPtr planSnap(
+  const aikido::statespace::StateSpace::State *startState,
+  const aikido::statespace::StateSpace::State *goalState,
+  const std::shared_ptr<aikido::statespace::StateSpace> stateSpace,
+  const std::shared_ptr<aikido::constraint::TestableConstraint> constraint,
+  const std::shared_ptr<aikido::statespace::Interpolator> interpolator,
+  aikido::planner::PlanningResult *planningResult);
 
-  returnTraj->addWaypoint(0, startState);
-  returnTraj->addWaypoint(1, goalState);
-  return returnTraj;
-}
-
-}  // planner
-}  // aikido
+} // namespace planner
+} // namespace aikido
 
 #endif  // AIKIDO_PLANNER_SNAP_PLANNER_HPP_
---
+++ aikido/src/planner/SnapPlanner.cpp
@@ -0,0 +1,43 @@
+#include <aikido/constraint/TestableConstraint.hpp>
+#include <aikido/path/PiecewiseLinearTrajectory.hpp>
+#include <aikido/planner/PlanningResult.hpp>
+#include <aikido/planner/SnapPlanner.hpp>
+#include <aikido/statespace/Interpolator.hpp>
+#include <aikido/statespace/StateSpace.hpp>
+#include <aikido/util/VanDerCorput.hpp>
+
+namespace aikido {
+namespace planner {
+
+path::PiecewiseLinearTrajectoryPtr planSnap(
+  const aikido::statespace::StateSpace::State *startState,
+  const aikido::statespace::StateSpace::State *goalState,
+  const std::shared_ptr<aikido::statespace::StateSpace> stateSpace,
+  const std::shared_ptr<aikido::constraint::TestableConstraint> constraint,
+  const std::shared_ptr<aikido::statespace::Interpolator> interpolator,
+  aikido::planner::PlanningResult *planningResult)
+{
+  if (stateSpace != constraint->getStateSpace()) {
+    throw std::invalid_argument(
+        "StateSpace of constraint not equal to StateSpace of planning space");
+  }
+  aikido::util::VanDerCorput vdc{1, true, 0.02};  // TODO junk resolution
+  auto returnTraj = std::make_shared<aikido::path::PiecewiseLinearTrajectory>(
+      stateSpace, interpolator);
+  auto testState = stateSpace->createState();
+
+  for (const auto alpha : vdc) {
+    interpolator->interpolate(startState, goalState, alpha, testState);
+    if (!constraint->isSatisfied(testState)) {
+      planningResult->message = "Collision detected";
+      return returnTraj;
+    }
+  }
+
+  returnTraj->addWaypoint(0, startState);
+  returnTraj->addWaypoint(1, goalState);
+  return returnTraj;
+}
+
+} // namespace planner
+} // namespace aikido
