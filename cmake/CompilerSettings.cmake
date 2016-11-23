c39a85f42f22e1a4d223d3e864ec222f93702c07
Jeongseok Lee <jslee02@gmail.com>
2016-11-23 11:02:44 -0500

    Split compiler settings into a helper file

---
+++ cmake/CompilerSettings.cmake
@@ -0,0 +1,65 @@
+#==============================================================================
+# Compiler settings
+#
+# Aikido currently supports four compilers: GCC, Clang (and AppleClang), and
+# MSVC. CompilerSettings optionally requires a custon CMake option
+# TREAT_WARNINGS_AS_ERRORS.
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+
+  # Aikido requires GCC 4.8 or greater to fully support C++11.
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 4.8)
+    message(FATAL_ERROR "The installed g++ version is
+        ${CMAKE_CXX_COMPILER_VERSION}. ${PROJECT_NAME} requires g++ 4.8 or
+        greater."
+    )
+  endif()
+
+  if(TREAT_WARNINGS_AS_ERRORS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
+  endif()
+
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+
+  # Aikido requires Clang 3.3 or greater to fully support C++11.
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 3.3)
+    message(FATAL_ERROR "The installed Clang version is
+        ${CMAKE_CXX_COMPILER_VERSION}. ${PROJECT_NAME} requires clang 3.3 or
+        greater."
+    )
+  endif()
+
+  if(TREAT_WARNINGS_AS_ERRORS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
+  endif()
+
+elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
+
+  # Aikido requires AppleClang 6.1 or greater to fully support C++11.
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 6.1)
+    message(FATAL_ERROR "The installed AppleClang version is
+        ${CMAKE_CXX_COMPILER_VERSION}. ${PROJECT_NAME} requires AppleClang 6.1
+        or greater."
+    )
+  endif()
+
+  if(TREAT_WARNINGS_AS_ERRORS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
+  endif()
+
+elseif(MSVC)
+
+  # Aikido requires Visual Studio 2015 or greater to fully support C++11.
+  if(MSVC_VERSION VERSION_LESS 1900)
+     message(FATAL_ERROR "${PROJECT_NAME} requires VS 2015 or greater.")
+   endif()
+
+  if(TREAT_WARNINGS_AS_ERRORS)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
+  endif()
+
+else()
+
+  message(SEND_ERROR "Compiler[${CMAKE_CXX_COMPILER_ID}] is not supported.")
+
+endif()
