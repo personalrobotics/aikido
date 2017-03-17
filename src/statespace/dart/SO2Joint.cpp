b07b104c2dd8c623c1287bd968d270724bb585bc
Jeongseok Lee <jslee02@gmail.com>
2017-03-17 15:39:45 -0400

    Merge remote-tracking branch 'origin/master' into feature/PrintCollisionResult
    Conflicts:
    	aikido/src/util/CMakeLists.txt

---
+++ src/statespace/dart/SO2Joint.cpp
@@ -0,0 +1,31 @@
+#include <aikido/statespace/dart/SO2Joint.hpp>
+
+namespace aikido {
+namespace statespace {
+namespace dart {
+
+//=============================================================================
+SO2Joint::SO2Joint(::dart::dynamics::GenericJoint<::dart::math::R1Space> *_joint)
+  : JointStateSpace(_joint)
+  , SO2()
+{
+}
+
+//=============================================================================
+void SO2Joint::convertPositionsToState(
+  const Eigen::VectorXd& _positions, StateSpace::State* _state) const
+{
+  setAngle(static_cast<State*>(_state), _positions[0]);
+}
+
+//=============================================================================
+void SO2Joint::convertStateToPositions(
+  const StateSpace::State* _state, Eigen::VectorXd& _positions) const
+{
+  _positions.resize(1);
+  _positions[0] = getAngle(static_cast<const State*>(_state));
+}
+
+} // namespace dart
+} // namespace statespace
+} // namespace aikido
