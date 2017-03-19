2b8419c0e620525e3e8f524c98c6c347373cb9b1
Jeongseok Lee <jslee02@gmail.com>
2017-03-19 16:54:02 -0400

    Attempt to fix build on linux

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -1,2 +1,2 @@
-mv "${TRAVIS_BUILD_DIR}" src
+cp "${TRAVIS_BUILD_DIR}" src
 ./scripts/internal-distro.py --workspace=src distribution.yml --repository "${REPOSITORY}"
--- ci/script_linux.sh
+++ ci/script_linux.sh
@@ -1,8 +1,8 @@
-- ./scripts/internal-build.sh ${PACKAGE_NAMES}
-- ./scripts/internal-test.sh ${PACKAGE_NAMES}
+./scripts/internal-build.sh ${PACKAGE_NAMES}
+./scripts/internal-test.sh ${PACKAGE_NAMES}
 
 # Manually run Aikido's tests; they are not run automatically because it is not a Catkin package.
-- ./scripts/internal-run.sh catkin build --no-status --no-deps -p 1 -i --make-args tests test -- aikido
+./scripts/internal-run.sh catkin build --no-status --no-deps -p 1 -i --make-args tests test -- aikido
 
 # Rebuild Aikido in debug mode with coveralls enabled to measure test coverage.
 ./scripts/internal-run.sh catkin clean -y aikido
