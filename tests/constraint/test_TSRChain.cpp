74284c770a2a578ce86d21e77ce0a8cfa2132d50
Daqing Yi <daqingy@ava.personalrobotics.ri.cmu.edu>
2017-04-03 13:48:16 -0400

    add test for TSR Chain

---
+++ tests/constraint/test_TSRChain.cpp
@@ -0,0 +1,20 @@
+#include <aikido/constraint/TSRChain.hpp>
+
+using aikido::constraint::TSRChain;
+
+TEST(TSRChain, CopyConstructor)
+{
+  TSRChain tsr_chain;
+  TSRChain tsr_chain2( tsr_chain );
+
+  EXPECT_EQ(tsr_chain.mTSRs.size(), tsr_chain2.mTSRs.size());
+  for(unsigned int i=0; i < tsr_chain.mTSRs.size(); i++)
+  {
+    EXPECT_EQ(tsr_chain.mTSRs[i].mT0_w, 
+              tsr_chain2.mTSRs[i].mT0_w);
+    EXPECT_EQ(tsr_chain.mTSRs[i].mBw, 
+              tsr_chain2.mTSRs[i].mBw);
+    EXPECT_EQ(tsr_chain.mTSRs[i].mTw_e, 
+              tsr_chain2.mTSRs[i].mTw_e);
+  }
+}
