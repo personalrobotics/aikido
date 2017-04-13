289e2df4fdf5ec2af8392bc4e726bf3376a827f6
Jeongseok Lee <jslee02@gmail.com>
2017-04-13 10:58:56 -0400

    Format code perception headers

--- include/aikido/perception/shape_conversions.hpp
+++ include/aikido/perception/shape_conversions.hpp
@@ -1,10 +1,10 @@
-#ifndef AIKIDO_PERCEPTION_SHAPE_CONVERSIONS_H
-#define AIKIDO_PERCEPTION_SHAPE_CONVERSIONS_H
+#ifndef AIKIDO_PERCEPTION_SHAPE_CONVERSIONS_HPP_
+#define AIKIDO_PERCEPTION_SHAPE_CONVERSIONS_HPP_
 
 #include <Eigen/Dense>
 #include <geometry_msgs/Pose.h>
-#include <tf/LinearMath/Vector3.h>
 #include <tf/LinearMath/Quaternion.h>
+#include <tf/LinearMath/Vector3.h>
 #include <tf/transform_listener.h>
 
 namespace aikido {
@@ -14,16 +14,15 @@ namespace perception {
 /// Convert a ROS Pose (position + quaternion) to a 4 x 4 pose matrix
 /// \param[in] p the geometry_msgs::Pose pose value
 /// return the 4 x 4 homogeneous matrix for the pose
-Eigen::Isometry3d convertROSPoseToEigen(geometry_msgs::Pose const &p);
+Eigen::Isometry3d convertROSPoseToEigen(geometry_msgs::Pose const& p);
 
 /// Convert a time-stamped ROS transform to a 4 x 4 pose matrix
 /// \param[in] t the time-stamped ROS transform (position + quaternion)
-/// \return the 4 x 4 homogeneous matrix corresponding to the time-stamped transform
-Eigen::Isometry3d convertStampedTransformToEigen(tf::StampedTransform const &t);
+/// \return the 4 x 4 homogeneous matrix corresponding to the time-stamped
+/// transform
+Eigen::Isometry3d convertStampedTransformToEigen(tf::StampedTransform const& t);
 
+} // namespace perception
+} // namespace aikido
 
-} //namespace perception
-
-} //namespace aikido
-
-#endif //AIKIDO_PERCEPTION_SHAPE_CONVERSIONS_H
+#endif // AIKIDO_PERCEPTION_SHAPE_CONVERSIONS_HPP_
