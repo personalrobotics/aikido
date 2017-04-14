7da6f1d84b382e55039b6bd9fee6af0150aaa315
Jeongseok Lee <jslee02@gmail.com>
2017-04-13 22:59:53 -0400

    Format distance code

--- src/distance/defaults.cpp
+++ src/distance/defaults.cpp
@@ -5,17 +5,16 @@ namespace distance {
 
 //=============================================================================
 std::unique_ptr<DistanceMetric> createDistanceMetric(
-  statespace::StateSpacePtr _sspace)
+    statespace::StateSpacePtr _sspace)
 {
   if (_sspace == nullptr)
     throw std::invalid_argument("_sspace is null.");
 
-  return util::DynamicCastFactory<
-      detail::createDistanceMetricFor_impl,
-      util::DynamicCastFactory_shared_ptr,
-      statespace::StateSpace,
-      detail::SupportedStateSpaces
-    >::create(_sspace);
+  return util::DynamicCastFactory<detail::createDistanceMetricFor_impl,
+                                  util::DynamicCastFactory_shared_ptr,
+                                  statespace::StateSpace,
+                                  detail::SupportedStateSpaces>::
+      create(_sspace);
 }
 
 } // namespace distance
