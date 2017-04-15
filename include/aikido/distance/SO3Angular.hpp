1accfe6c067afae7f39627f52536b62e97d1cba0
Jeongseok Lee <jslee02@users.noreply.github.com>
2017-04-15 07:15:21 -0400

    Code format: distance (#172)

--- include/aikido/distance/SO3Angular.hpp
+++ include/aikido/distance/SO3Angular.hpp
@@ -1,21 +1,19 @@
 #ifndef AIKIDO_DISTANCE_GEODESICDISTANCEMETRIC_HPP_
 #define AIKIDO_DISTANCE_GEODESICDISTANCEMETRIC_HPP_
 
-#include "DistanceMetric.hpp"
 #include "../statespace/SO3.hpp"
+#include "DistanceMetric.hpp"
 
-namespace aikido
-{
-namespace distance
-{
+namespace aikido {
+namespace distance {
+
 /// Implements a distance metric on SO(3)
 class SO3Angular : public DistanceMetric
 {
 public:
   /// Constructor.
   /// \param _space The SO3 this distance metric operates on
-  explicit SO3Angular(
-      std::shared_ptr<statespace::SO3> _space);
+  explicit SO3Angular(std::shared_ptr<statespace::SO3> _space);
 
   // Documentation inherited
   statespace::StateSpacePtr getStateSpace() const override;
@@ -23,12 +21,15 @@ public:
   /// Computes distance (in radians) between the two states
   /// \param _state1 The first state (type SO3::State)
   /// \param _state2 The second state (type SO3::State)
-  double distance(const statespace::StateSpace::State* _state1,
-                  const statespace::StateSpace::State* _state2) const override;
+  double distance(
+      const statespace::StateSpace::State* _state1,
+      const statespace::StateSpace::State* _state2) const override;
 
 private:
   std::shared_ptr<statespace::SO3> mStateSpace;
 };
-}
-}
+
+} // namespace distance
+} // namespace aikido
+
 #endif
