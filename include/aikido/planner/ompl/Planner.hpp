c7a046b0756cc25eac128fd31c8616e99c725477
aditya-vk <adityavk711@gmail.com>
2017-04-15 20:23:59 -0400

    function declarations for helper functions

--- include/aikido/planner/ompl/Planner.hpp
+++ include/aikido/planner/ompl/Planner.hpp
@@ -309,6 +309,16 @@ std::pair <std::unique_ptr<trajectory::Interpolated>, bool> simplifyOMPL(statesp
                                                         double _timeout, size_t _maxEmptySteps,
                                                         trajectory::InterpolatedPtr _originalTraj);
 
+
+::ompl::geometric::PathGeometric toOMPLTrajectory(trajectory::InterpolatedPtr _interpolatedTraj,
+                                                  ::ompl::base::SpaceInformationPtr _si
+                                                  );
+
+
+std::unique_ptr<trajectory::Interpolated> toInterpolatedTrajectory(::ompl::geometric::PathGeometric const *_path,
+                                                                  statespace::StateSpacePtr _stateSpace,
+                                                                  statespace::InterpolatorPtr _interpolator);
+
 } // namespace ompl
 } // namespace planner
 } // namespace aikido
