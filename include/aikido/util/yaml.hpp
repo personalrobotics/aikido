4f0a42829a49b74d77055a77bc254ba41c6fedc9
Jeongseok Lee <jslee02@gmail.com>
2017-04-19 00:04:19 -0400

    Add comment

--- include/aikido/util/yaml.hpp
+++ include/aikido/util/yaml.hpp
@@ -2,6 +2,14 @@
 #define AIKIDO_UTIL_YAML_HPP_
 
 #include <yaml-cpp/yaml.h>
+
 #include "aikido/util/detail/yaml_eigen_extension.hpp"
+// The above header allows extended value types for yaml-cpp.
+//
+// Supported types:
+// - Eigen::Matrix<...>
+// - Eigen::Isometry<...>
+// - std::unordered_map
+//
 
 #endif // AIKIDO_UTIL_YAML_HPP_
