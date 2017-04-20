5bd3daf92513c0faead43f193c4d42544b0e26db
aditya-vk <adityavk711@gmail.com>
2017-04-20 00:21:40 -0400

    adding required header files for simplifier test

--- tests/planner/ompl/test_OMPLSimplifier.cpp
+++ tests/planner/ompl/test_OMPLSimplifier.cpp
@@ -7,9 +7,15 @@
 #include <aikido/constraint/JointStateSpaceHelpers.hpp>
 #include <aikido/util/StepSequence.hpp>
 #include <aikido/planner/ompl/MotionValidator.hpp>
+#include <aikido/planner/ompl/CRRT.hpp>
+#include <aikido/planner/ompl/CRRTConnect.hpp>
+#include <ompl/geometric/planners/rrt/RRTConnect.h>
+
 
 using StateSpace = aikido::statespace::dart::MetaSkeletonStateSpace;
 using aikido::planner::ompl::getSpaceInformation;
+using aikido::planner::ompl::CRRT;
+using aikido::planner::ompl::CRRTConnect;
 
 TEST_F(SimplifierTest, EndPointCheck)
 {
