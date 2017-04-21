68d7120417a9489c33bab13184cfae3b56819f09
Aditya Vamsikrishna <adityavk711@gmail.com>
2017-04-21 02:12:38 -0400

    test for if the path REALLY got shortened

--- tests/planner/ompl/test_OMPLSimplifier.cpp
+++ tests/planner/ompl/test_OMPLSimplifier.cpp
@@ -16,6 +16,7 @@ using aikido::planner::ompl::getSpaceInformation;
 using aikido::planner::ompl::CRRT;
 using aikido::planner::ompl::CRRTConnect;
 
+
 TEST_F(SimplifierTest, StartAndEndStatesUnchanged)
 {
   Eigen::Vector3d startPose(-5, -5, 0);
@@ -35,6 +36,7 @@ TEST_F(SimplifierTest, StartAndEndStatesUnchanged)
       std::move(sampler), std::move(collConstraint),
       std::move(boundsConstraint), std::move(boundsProjection), 5.0, 0.1);
 
+  // TODO: unique pointers are lost. Is there a better way to go about this?
   aikido::distance::DistanceMetricPtr _dmetric = aikido::distance::createDistanceMetric(stateSpace);
   aikido::constraint::SampleablePtr _sampler = aikido::constraint::createSampleableBounds(stateSpace, make_rng()); 
   aikido::constraint::ProjectablePtr _boundsProjection = aikido::constraint::createProjectableBounds(stateSpace);
@@ -50,6 +52,8 @@ TEST_F(SimplifierTest, StartAndEndStatesUnchanged)
   auto simplifiedTraj = std::move(simplifiedPair.first);
   bool shorten_success = simplifiedPair.second;
   
+// Test that the start and goal positions do not change
+//=============================================================================
   // Check the first waypoint
   auto s0 = stateSpace->createState();
   simplifiedTraj->evaluate(0, s0);
@@ -60,4 +64,39 @@ TEST_F(SimplifierTest, StartAndEndStatesUnchanged)
   simplifiedTraj->evaluate(simplifiedTraj->getDuration(), s0);
   r0 = s0.getSubStateHandle<R3>(0);
   EXPECT_TRUE(r0.getValue().isApprox(goalPose));
+
+// Test that the simplification process returns the right notification to user
+//=============================================================================
+  double trajDistance = 0.0;
+  for(size_t i = 0; i < traj->getDuration()-1; ++i)
+  {
+    traj->evaluate(i, s0);
+    r0 = s0.getSubStateHandle<R3>(0);
+    auto currentState = r0.getValue();
+
+    traj->evaluate(i+1, s0);
+    r0 = s0.getSubStateHandle<R3>(0);
+    auto nextState = r0.getValue();
+
+    trajDistance += pow(nextState[0] - currentState[0], 2) + pow(nextState[1] - currentState[1], 2) + pow(nextState[2] - currentState[2], 2);
+  }
+  trajDistance = trajDistance / (traj -> getDuration());
+
+  double simplifiedTrajDistance = 0.0;
+  for(size_t i = 0; i < simplifiedTraj->getDuration()-1; ++i)
+  {
+    simplifiedTraj->evaluate(i, s0);
+    r0 = s0.getSubStateHandle<R3>(0);
+    auto currentState = r0.getValue();
+
+    simplifiedTraj->evaluate(i+1, s0);
+    r0 = s0.getSubStateHandle<R3>(0);
+    auto nextState = r0.getValue();
+
+    simplifiedTrajDistance += pow(nextState[0] - currentState[0], 2) + pow(nextState[1] - currentState[1], 2) + pow(nextState[2] - currentState[2], 2);
+  }
+  simplifiedTrajDistance = simplifiedTrajDistance / (simplifiedTraj->getDuration());
+
+  EXPECT_TRUE(shorten_success == ( simplifiedTrajDistance < trajDistance ));  
+  // EXPECT_TRUE(pow(startPose[0],2) == 25);
 }
