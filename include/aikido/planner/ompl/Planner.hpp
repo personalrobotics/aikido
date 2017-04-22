73f50b71490a521f8461866826ceacbab65fe583
Aditya Vamsikrishna <adityavk711@gmail.com>
2017-04-22 00:17:26 -0400

    satisfy travis with code formatting

--- include/aikido/planner/ompl/Planner.hpp
+++ include/aikido/planner/ompl/Planner.hpp
@@ -312,11 +312,11 @@ std::pair<std::unique_ptr<trajectory::Interpolated>, bool> simplifyOMPL(
     trajectory::InterpolatedPtr _originalTraj);
 
 ::ompl::geometric::PathGeometric toOMPLTrajectory(
-    const trajectory::InterpolatedPtr &_interpolatedTraj,
+    const trajectory::InterpolatedPtr& _interpolatedTraj,
     ::ompl::base::SpaceInformationPtr _si);
 
 std::unique_ptr<trajectory::Interpolated> toInterpolatedTrajectory(
-    const ::ompl::geometric::PathGeometric &_path,
+    const ::ompl::geometric::PathGeometric& _path,
     statespace::StateSpacePtr _stateSpace,
     statespace::InterpolatorPtr _interpolator);
 
