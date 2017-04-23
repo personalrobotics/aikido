8b668108f38f0fc01433effad9187a8039776356
Aditya Vamsikrishna <adityavk711@gmail.com>
2017-04-23 02:49:54 -0400

    cleaned tests for simplifier

--- tests/planner/ompl/test_OMPLSimplifier.cpp
+++ tests/planner/ompl/test_OMPLSimplifier.cpp
@@ -45,48 +45,67 @@ namespace {
     return trajDistance;
   }
 
-  // aikido::trajectory::InterpolatedPtr planTrajectoryForTest(aikido::statespace::dart::MetaSkeletonStateSpacePtr stateSpace,
-  //     )
+  aikido::trajectory::InterpolatedPtr planTrajForTest(
+      aikido::statespace::dart::MetaSkeletonStateSpacePtr _stateSpace,
+      aikido::statespace::InterpolatorPtr& _interpolator,
+      aikido::distance::DistanceMetricPtr& _dmetric,
+      aikido::constraint::SampleablePtr& _sampler,
+      aikido::constraint::TestablePtr& _collConstraint,
+      aikido::constraint::TestablePtr& _boundsConstraint,
+      aikido::constraint::ProjectablePtr& _boundsProjection,
+      Eigen::Vector3d _startPose, Eigen::Vector3d _goalPose          
+    )
+  {
+
+    auto startState = _stateSpace->createState();
+    auto subState1 = _stateSpace->getSubStateHandle<R3>(startState, 0);
+    subState1.setValue(_startPose);
+
+    auto goalState = _stateSpace->createState();
+    auto subState2 = _stateSpace->getSubStateHandle<R3>(goalState, 0);
+    subState2.setValue(_goalPose);
+
+    // Plan
+    auto traj = aikido::planner::ompl::planOMPL<ompl::geometric::RRTConnect>(
+        startState, goalState, _stateSpace, _interpolator, std::move(_dmetric),
+        std::move(_sampler), std::move(_collConstraint),
+        std::move(_boundsConstraint), std::move(_boundsProjection), 5.0, 0.1);
+
+    return traj;
+
+  }
 
 } // nampespace
 
+
+// Test that the start and goal positions do not change
 TEST_F(SimplifierTest, EndPointsRemainUnchanged)
 {
   Eigen::Vector3d startPose(-5, -5, 0);
   Eigen::Vector3d goalPose(5, 5, 0);
 
-  auto startState = stateSpace->createState();
-  auto subState1 = stateSpace->getSubStateHandle<R3>(startState, 0);
-  subState1.setValue(startPose);
+  // Original Trajectory
+  auto traj = planTrajForTest(stateSpace, interpolator, dmetric, sampler, collConstraint, 
+      boundsConstraint, boundsProjection, startPose, goalPose);
 
-  auto goalState = stateSpace->createState();
-  auto subState2 = stateSpace->getSubStateHandle<R3>(goalState, 0);
-  subState2.setValue(goalPose);
-
-  // Plan
-  auto traj = aikido::planner::ompl::planOMPL<ompl::geometric::RRTConnect>(
-      startState, goalState, stateSpace, interpolator, std::move(dmetric),
-      std::move(sampler), std::move(collConstraint),
-      std::move(boundsConstraint), std::move(boundsProjection), 5.0, 0.1);
-
-  // TODO: unique pointers are lost. Is there a better way to go about this?
-  aikido::distance::DistanceMetricPtr _dmetric = aikido::distance::createDistanceMetric(stateSpace);
-  aikido::constraint::SampleablePtr _sampler = aikido::constraint::createSampleableBounds(stateSpace, make_rng()); 
-  aikido::constraint::ProjectablePtr _boundsProjection = aikido::constraint::createProjectableBounds(stateSpace);
-  aikido::constraint::TestablePtr _boundsConstraint = aikido::constraint::createTestableBounds(stateSpace);
-  aikido::constraint::TestablePtr _collConstraint = std::make_shared<MockTranslationalRobotConstraint>(
+  // Setup for simplification
+  aikido::distance::DistanceMetricPtr dmetric = aikido::distance::createDistanceMetric(stateSpace);
+  aikido::constraint::SampleablePtr sampler = aikido::constraint::createSampleableBounds(stateSpace, make_rng()); 
+  aikido::constraint::ProjectablePtr boundsProjection = aikido::constraint::createProjectableBounds(stateSpace);
+  aikido::constraint::TestablePtr boundsConstraint = aikido::constraint::createTestableBounds(stateSpace);
+  aikido::constraint::TestablePtr collConstraint = std::make_shared<MockTranslationalRobotConstraint>(
         stateSpace, Eigen::Vector3d(-0.1, -0.1, -0.1),
         Eigen::Vector3d(0.1, 0.1, 0.1));
 
   auto simplifiedPair = aikido::planner::ompl::simplifyOMPL(
-      stateSpace, interpolator, std::move(_dmetric), std::move(_sampler), 
-      std::move(_collConstraint), std::move(_boundsConstraint), std::move(_boundsProjection),
+      stateSpace, interpolator, std::move(dmetric), std::move(sampler), 
+      std::move(collConstraint), std::move(boundsConstraint), std::move(boundsProjection),
       0.1, 5.0, 10, traj);
+
+  // Simplification results
   auto simplifiedTraj = std::move(simplifiedPair.first);
   bool shorten_success = simplifiedPair.second;
-  
-// Test that the start and goal positions do not change
-//=============================================================================
+
   // Check the first waypoint
   auto s0 = stateSpace->createState();
   simplifiedTraj->evaluate(0, s0);
@@ -100,45 +119,35 @@ TEST_F(SimplifierTest, EndPointsRemainUnchanged)
 
 }
 
+
+// Test that the boolean returned is in agreement with shortening process
 TEST_F(SimplifierTest, ShortenThreeWayPointTraj)
 {
 
   Eigen::Vector3d startPose(-5, -5, 0);
   Eigen::Vector3d goalPose(5, 5, 0);
 
-  auto startState = stateSpace->createState();
-  auto subState1 = stateSpace->getSubStateHandle<R3>(startState, 0);
-  subState1.setValue(startPose);
+  auto traj = planTrajForTest(stateSpace, interpolator, dmetric, sampler, collConstraint, 
+      boundsConstraint, boundsProjection, startPose, goalPose);
 
-  auto goalState = stateSpace->createState();
-  auto subState2 = stateSpace->getSubStateHandle<R3>(goalState, 0);
-  subState2.setValue(goalPose);
-
-  // Plan
-  auto traj = aikido::planner::ompl::planOMPL<ompl::geometric::RRTConnect>(
-      startState, goalState, stateSpace, interpolator, std::move(dmetric),
-      std::move(sampler), std::move(collConstraint),
-      std::move(boundsConstraint), std::move(boundsProjection), 5.0, 0.1);
-
-  // TODO: unique pointers are lost. Is there a better way to go about this?
-  aikido::distance::DistanceMetricPtr _dmetric = aikido::distance::createDistanceMetric(stateSpace);
-  aikido::constraint::SampleablePtr _sampler = aikido::constraint::createSampleableBounds(stateSpace, make_rng()); 
-  aikido::constraint::ProjectablePtr _boundsProjection = aikido::constraint::createProjectableBounds(stateSpace);
-  aikido::constraint::TestablePtr _boundsConstraint = aikido::constraint::createTestableBounds(stateSpace);
-  aikido::constraint::TestablePtr _collConstraint = std::make_shared<MockTranslationalRobotConstraint>(
+  // Setup for simplification
+  aikido::distance::DistanceMetricPtr dmetric = aikido::distance::createDistanceMetric(stateSpace);
+  aikido::constraint::SampleablePtr sampler = aikido::constraint::createSampleableBounds(stateSpace, make_rng()); 
+  aikido::constraint::ProjectablePtr boundsProjection = aikido::constraint::createProjectableBounds(stateSpace);
+  aikido::constraint::TestablePtr boundsConstraint = aikido::constraint::createTestableBounds(stateSpace);
+  aikido::constraint::TestablePtr collConstraint = std::make_shared<MockTranslationalRobotConstraint>(
         stateSpace, Eigen::Vector3d(-0.1, -0.1, -0.1),
-        Eigen::Vector3d(0.1, 0.1, 0.1));  
+        Eigen::Vector3d(0.1, 0.1, 0.1));
 
   auto simplifiedPair = aikido::planner::ompl::simplifyOMPL(
-      stateSpace, interpolator, std::move(_dmetric), std::move(_sampler), 
-      std::move(_collConstraint), std::move(_boundsConstraint), std::move(_boundsProjection),
+      stateSpace, interpolator, std::move(dmetric), std::move(sampler), 
+      std::move(collConstraint), std::move(boundsConstraint), std::move(boundsProjection),
       0.1, 5.0, 10, traj);
+
+  // Simplification results
   auto simplifiedTraj = std::move(simplifiedPair.first);
   bool shorten_success = simplifiedPair.second;
 
-
-// Test that the boolean returned is in agreement with shortening process
-//=============================================================================
   aikido::distance::DistanceMetricPtr Ddmetric = aikido::distance::createDistanceMetric(stateSpace);
   double trajDistance = computeTrajLength<aikido::trajectory::InterpolatedPtr>(traj, stateSpace, Ddmetric);
   double simplifiedTrajDistance = computeTrajLength<std::unique_ptr<aikido::trajectory::Interpolated>>(simplifiedTraj, stateSpace, Ddmetric);
@@ -146,6 +155,8 @@ TEST_F(SimplifierTest, ShortenThreeWayPointTraj)
   EXPECT_TRUE(shorten_success == (simplifiedTrajDistance < trajDistance));  
 }
 
+
+// Test that a path with two waypoints (straight line) does not get shortened
 TEST_F(SimplifierTest, ShortenTwoWayPointTraj)
 {
 
