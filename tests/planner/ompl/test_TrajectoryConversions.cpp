5255dd1f3c9d14b35de30b5496b793069bfa68bb
Aditya Vamsikrishna <adityavk711@gmail.com>
2017-04-23 13:36:27 -0400

    seperated tests. added helper functions

--- tests/planner/ompl/test_TrajectoryConversions.cpp
+++ tests/planner/ompl/test_TrajectoryConversions.cpp
@@ -16,26 +16,88 @@ using aikido::planner::ompl::getSpaceInformation;
 using aikido::planner::ompl::CRRT;
 using aikido::planner::ompl::CRRTConnect;
 
-TEST_F(PlannerTest, TrajectoryConversionsTest)
+namespace {
+
+  aikido::trajectory::InterpolatedPtr planTrajForTest(
+      aikido::statespace::dart::MetaSkeletonStateSpacePtr _stateSpace,
+      aikido::statespace::InterpolatorPtr& _interpolator,
+      aikido::distance::DistanceMetricPtr& _dmetric,
+      aikido::constraint::SampleablePtr& _sampler,
+      aikido::constraint::TestablePtr& _collConstraint,
+      aikido::constraint::TestablePtr& _boundsConstraint,
+      aikido::constraint::ProjectablePtr& _boundsProjection,
+      Eigen::Vector3d _startPose, Eigen::Vector3d _goalPose          
+    )
+  {
+
+    auto startState = _stateSpace->createState();
+    auto subState1 = _stateSpace->getSubStateHandle<R3>(startState, 0);
+    subState1.setValue(_startPose);
+
+    auto goalState = _stateSpace->createState();
+    auto subState2 = _stateSpace->getSubStateHandle<R3>(goalState, 0);
+    subState2.setValue(_goalPose);
+
+    // Plan
+    auto traj = aikido::planner::ompl::planOMPL<ompl::geometric::RRTConnect>(
+        startState, goalState, _stateSpace, _interpolator, std::move(_dmetric),
+        std::move(_sampler), std::move(_collConstraint),
+        std::move(_boundsConstraint), std::move(_boundsProjection), 5.0, 0.1);
+
+    return traj;
+
+  }
+
+}
+
+TEST_F(PlannerTest, DurationRemainsUnchanged)
 {
   Eigen::Vector3d startPose(-5, -5, 0);
   Eigen::Vector3d goalPose(5, 5, 0);
 
-  auto startState = stateSpace->createState();
-  auto subState1 = stateSpace->getSubStateHandle<R3>(startState, 0);
-  subState1.setValue(startPose);
+  // Original Trajectory
+  auto traj = planTrajForTest(stateSpace, interpolator, dmetric, sampler, collConstraint, 
+      boundsConstraint, boundsProjection, startPose, goalPose);
 
-  auto goalState = stateSpace->createState();
-  auto subState2 = stateSpace->getSubStateHandle<R3>(goalState, 0);
-  subState2.setValue(goalPose);
+  // Setup
+  aikido::distance::DistanceMetricPtr _dmetric = aikido::distance::createDistanceMetric(stateSpace);
+  aikido::constraint::SampleablePtr _sampler = aikido::constraint::createSampleableBounds(stateSpace, make_rng()); 
+  aikido::constraint::ProjectablePtr _boundsProjection = aikido::constraint::createProjectableBounds(stateSpace);
+  aikido::constraint::TestablePtr _boundsConstraint = aikido::constraint::createTestableBounds(stateSpace);
+  aikido::constraint::TestablePtr _collConstraint = std::make_shared<MockTranslationalRobotConstraint>(
+        stateSpace, Eigen::Vector3d(-0.1, -0.1, -0.1),
+        Eigen::Vector3d(0.1, 0.1, 0.1));
 
-  // Plan
-  auto traj = aikido::planner::ompl::planOMPL<ompl::geometric::RRTConnect>(
-      startState, goalState, stateSpace, interpolator, std::move(dmetric),
-      std::move(sampler), std::move(collConstraint),
-      std::move(boundsConstraint), std::move(boundsProjection), 5.0, 0.1);
+  // Get the ompl state space
+  auto si = getSpaceInformation(
+      stateSpace,
+      interpolator,
+      std::move(_dmetric),
+      std::move(_sampler),
+      std::move(_collConstraint),
+      std::move(_boundsConstraint),
+      std::move(_boundsProjection),
+      0.1);
 
-  // Conversions
+  auto omplTraj = aikido::planner::ompl::toOMPLTrajectory(
+      traj, si);
+
+  // Note that getDuration() indexes from 0 while getStateCount() indexes from 1
+  EXPECT_TRUE(omplTraj.getStateCount() == (traj->getDuration())+1);
+
+}
+
+
+TEST_F(PlannerTest, StatesRemainUnchaged)
+{
+  Eigen::Vector3d startPose(-5, -5, 0);
+  Eigen::Vector3d goalPose(5, 5, 0);
+
+  // Original Trajectory
+  auto traj = planTrajForTest(stateSpace, interpolator, dmetric, sampler, collConstraint, 
+      boundsConstraint, boundsProjection, startPose, goalPose);
+
+  // Setup
   aikido::distance::DistanceMetricPtr _dmetric = aikido::distance::createDistanceMetric(stateSpace);
   aikido::constraint::SampleablePtr _sampler = aikido::constraint::createSampleableBounds(stateSpace, make_rng()); 
   aikido::constraint::ProjectablePtr _boundsProjection = aikido::constraint::createProjectableBounds(stateSpace);
@@ -59,42 +121,22 @@ TEST_F(PlannerTest, TrajectoryConversionsTest)
       traj, si);
 
   auto interpolatedTraj = aikido::planner::ompl::toInterpolatedTrajectory(
-    omplTraj, stateSpace, interpolator);
-
-  // Check the first waypoint
-  auto s0 = stateSpace->createState();
-  traj->evaluate(0, s0);
-  auto r0 = s0.getSubStateHandle<R3>(0);
-  EXPECT_TRUE(r0.getValue().isApprox(startPose));
-
-  s0 = stateSpace->createState();
-  interpolatedTraj->evaluate(0, s0);
-  r0 = s0.getSubStateHandle<R3>(0);
-  EXPECT_TRUE(r0.getValue().isApprox(startPose));
-
-  // Check the last waypoint
-  traj->evaluate(traj->getDuration(), s0);
-  r0 = s0.getSubStateHandle<R3>(0);
-  EXPECT_TRUE(r0.getValue().isApprox(goalPose));
-
-  interpolatedTraj->evaluate(interpolatedTraj->getDuration(), s0);
-  r0 = s0.getSubStateHandle<R3>(0);
-  EXPECT_TRUE(r0.getValue().isApprox(goalPose));
-
-  // Check Duration
-  EXPECT_TRUE(traj->getDuration() == interpolatedTraj->getDuration());
+  omplTraj, stateSpace, interpolator);
 
   // Match every point
-  for(size_t i = 0; i < traj->getDuration(); ++i)
+  auto s0 = stateSpace->createState();
+  auto s1 = stateSpace->createState();
+  for(size_t idx = 0; idx < traj->getDuration(); ++idx)
   {
-    traj->evaluate(traj->getDuration(), s0);
-    r0 = s0.getSubStateHandle<R3>(0);
+    traj->evaluate(idx, s0);
+    auto r0 = s0.getSubStateHandle<R3>(0);
     auto stateBefore = r0.getValue();
 
-    interpolatedTraj->evaluate(interpolatedTraj->getDuration(), s0);
-    r0 = s0.getSubStateHandle<R3>(0);
-    auto stateAfter = r0.getValue();
+    interpolatedTraj->evaluate(idx, s1);
+    auto r1 = s1.getSubStateHandle<R3>(0);
+    auto stateAfter = r1.getValue();
 
     EXPECT_TRUE(stateBefore.isApprox(stateAfter));
   }
+
 }
