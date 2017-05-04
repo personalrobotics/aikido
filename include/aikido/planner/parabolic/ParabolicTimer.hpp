65606f91e1b2b83588ac5b3e56142ad79af1332a
Daqing Yi <daqingy@ava.personalrobotics.ri.cmu.edu>
2017-05-04 11:34:20 -0400

    address JS's comments

--- include/aikido/planner/parabolic/ParabolicTimer.hpp
+++ include/aikido/planner/parabolic/ParabolicTimer.hpp
@@ -35,8 +35,8 @@ std::unique_ptr<aikido::trajectory::Spline> computeParabolicTiming(
     const Eigen::VectorXd& _maxAcceleration);
 
 /// Convert an interpolated trajectory to a spline trajectory
-/// This function requires the \c _inputTrajectory to use a \c
-/// GeodesicInterpolator.
+/// This function requires the \c _inputTrajectory to use a 
+/// \c GeodesicInterpolator.
 /// So the conversion exactly preserves the geometric path
 /// \param _inputTrajectory interpolated trajectory
 /// \return a spline trajectory
