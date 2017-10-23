d93d820c22cc66c06fb121f70f23fd2379d4769d
Brian Hou <brianmhou@gmail.com>
2017-10-22 19:15:01 -0700

    Document WorldInteractiveMarkerViewer.

--- include/aikido/rviz/WorldInteractiveMarkerViewer.hpp
+++ include/aikido/rviz/WorldInteractiveMarkerViewer.hpp
@@ -12,6 +12,10 @@ namespace rviz {
 class WorldInteractiveMarkerViewer : public InteractiveMarkerViewer
 {
 public:
+  /// Create an InteractiveMarkerViewer that reflects skeletons in a World.
+  /// \param env World to update viewer with
+  /// \param topicNamespace ROS topic to publish marker updates to
+  /// \param frameId Base frame name
   WorldInteractiveMarkerViewer(
       aikido::planner::WorldPtr env,
       const std::string& topicNamespace,
@@ -23,13 +27,21 @@ public:
   WorldInteractiveMarkerViewer& operator=(const WorldInteractiveMarkerViewer&)
       = delete;
 
+  /// Set viewer auto-updating
+  /// \param flag Whether to auto-update the viewer
   void setAutoUpdate(bool flag);
+
+  /// Update viewer with Skeletons from the World
   void update();
 
 protected:
+  /// Thread target for auto-updating the viewer
   void autoUpdate();
 
+  /// Mapping of Skeleton names to SkeletonMarkers
   std::map<std::string, SkeletonMarkerPtr> mSkeletonMarkers;
+
+  /// World that automatically updates the viewer
   aikido::planner::WorldPtr mWorld;
 };
 
