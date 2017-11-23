fd575869c5d48dc62fb7817eba651e2b5ac2d135
Daqing Yi <dqyi11@gmail.com>
2017-11-23 00:01:09 -0800

    Avoid invalid argument exceptions in computeJointVelocityFromTwist (#262)
    * add initial guess for nlopt
    
    * code formatting
    
    * update CHANGELOG.md
    
    * Add clamp() function
    
    and try to improve the readability of computeJointVelocityFromTwist()
    
    * remove unncessary warning

---
+++ include/aikido/common/detail/algorithm-impl.hpp
@@ -0,0 +1,30 @@
+#ifndef AIKIDO_COMMON_DETAIL_ALGORITHM_IMPL_HPP_
+#define AIKIDO_COMMON_DETAIL_ALGORITHM_IMPL_HPP_
+
+#include "aikido/common/algorithm.hpp"
+
+#include <cassert>
+#include <functional>
+
+namespace aikido {
+namespace common {
+
+//==============================================================================
+template <class T, class Compare>
+const T& clamp(const T& v, const T& lo, const T& hi, Compare comp)
+{
+  assert(!comp(hi, lo));
+  return comp(v, lo) ? lo : comp(hi, v) ? hi : v;
+}
+
+//==============================================================================
+template <class T>
+const T& clamp(const T& v, const T& lo, const T& hi)
+{
+  return clamp(v, lo, hi, std::less<T>());
+}
+
+} // namespace common
+} // namespace aikido
+
+#endif // AIKIDO_COMMON_DETAIL_ALGORITHM_IMPL_HPP_
