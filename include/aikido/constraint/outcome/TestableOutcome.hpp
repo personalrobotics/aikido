3fcb61f5474765751e7540884b5027e10489b1fa
Sherdil Niyaz <sniyaz@berkeley.edu>
2017-11-25 22:00:47 -0500

    Added TestableOutcomeClass.

---
+++ include/aikido/constraint/outcome/TestableOutcome.hpp
@@ -0,0 +1,16 @@
+#ifndef AIKIDO_CONSTRAINT_TESTABLEOUTCOME_HPP_
+#define AIKIDO_CONSTRAINT_TESTABLEOUTCOME_HPP_
+
+namespace aikido {
+namespace constraint {
+
+class TestableOutcome {
+public:
+  bool is_satisfied() const = 0;
+  virtual std::string to_string() const = 0;
+};
+
+} // namespace constraint
+} // namespace aikido
+
+#endif // ifndef AIKIDO_CONSTRAINT_TESTABLEOUTCOME_HPP_
