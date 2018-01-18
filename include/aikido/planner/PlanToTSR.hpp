4be260e8bfcba6a5a17a3879f9ada7956a4cc10a
Aditya Vamsikrishna <adityavk@uw.edu>
2018-01-17 16:09:15 -0800

    planToTSR added

---
+++ include/aikido/planner/PlanToTSR.hpp
@@ -0,0 +1,62 @@
+#ifndef AIKIDO_PLANNER_PLANTOTSR_HPP_
+#define AIKIDO_PLANNER_PLANTOTSR_HPP_
+
+#include "aikido/planner/Problem.hpp"
+#include "aikido/statespace/StateSpace.hpp"
+#include "aikido/statespace/smart_pointer.hpp"
+#include "aikido/constraint/smart_pointer.hpp"
+#include "aikido/constraint/TSR.hpp"
+#include "aikido/trajectory/Interpolated.hpp"
+
+namespace aikido {
+namespace planner {
+
+/// Planning problem to plan to given Task Space Region (TSR).
+class PlanToTSR : public Problem
+{
+public:
+  using ReturnTrajectoryType = trajectory::Interpolated;
+
+  class Result;
+
+  PlanToTSR(
+      statespace::ConstStateSpacePtr stateSpace,
+      const statespace::StateSpace::State* startState,
+      const constraint::TSRPtr goalTSR,
+      statespace::InterpolatorPtr interpolator,
+      constraint::TestablePtr constraint);
+
+  const std::string& getName() const override;
+
+  static const std::string& getStaticName();
+
+  const statespace::StateSpace::State* getStartState() const;
+
+  const constraint::TSRPtr getGoalTSR() const;
+
+  statespace::InterpolatorPtr getInterpolator();
+
+  statespace::ConstInterpolatorPtr getInterpolator() const;
+
+  constraint::TestablePtr getConstraint();
+
+  constraint::ConstTestablePtr getConstraint() const;
+
+protected:
+  const statespace::StateSpace::State* mStartState;
+  const constraint::TSRPtr mGoalTSR;
+
+  statespace::InterpolatorPtr mInterpolator;
+  constraint::TestablePtr mConstraint;
+};
+
+class PlanToTSR::Result : public Problem::Result
+{
+public:
+protected:
+};
+
+} // namespace planner
+} // namespace aikido
+
+#endif
---
+++ src/planner/PlanToTSR.cpp
@@ -0,0 +1,78 @@
+#include "aikido/planner/PlanToTSR.hpp"
+
+#include "aikido/constraint/Testable.hpp"
+
+namespace aikido {
+namespace planner {
+
+//==============================================================================
+PlanToTSR::PlanToTSR(
+    statespace::ConstStateSpacePtr stateSpace,
+    const statespace::StateSpace::State* startState,
+    const constraint::TSRPtr goalTSR,
+    statespace::InterpolatorPtr interpolator,
+    constraint::TestablePtr constraint)
+  : Problem(std::move(stateSpace))
+  , mStartState(startState)
+  , mGoalTSR(goalTSR)
+  , mInterpolator(std::move(interpolator))
+  , mConstraint(std::move(constraint))
+{
+  if (mStateSpace != mConstraint->getStateSpace())
+  {
+    throw std::invalid_argument(
+        "StateSpace of constraint not equal to StateSpace of planning space");
+  }
+}
+
+//==============================================================================
+const std::string& PlanToTSR::getName() const
+{
+  return getStaticName();
+}
+
+//==============================================================================
+const std::string& PlanToTSR::getStaticName()
+{
+  static std::string name("PlanToTSR");
+  return name;
+}
+
+//==============================================================================
+const statespace::StateSpace::State* PlanToTSR::getStartState() const
+{
+  return mStartState;
+}
+
+//==============================================================================
+const constraint::TSRPtr PlanToTSR::getGoalTSR() const
+{
+  return mGoalTSR;
+}
+
+//==============================================================================
+statespace::InterpolatorPtr PlanToTSR::getInterpolator()
+{
+  return mInterpolator;
+}
+
+//==============================================================================
+statespace::ConstInterpolatorPtr PlanToTSR::getInterpolator() const
+{
+  return mInterpolator;
+}
+
+//==============================================================================
+constraint::TestablePtr PlanToTSR::getConstraint()
+{
+  return mConstraint;
+}
+
+//==============================================================================
+constraint::ConstTestablePtr PlanToTSR::getConstraint() const
+{
+  return mConstraint;
+}
+
+} // namespace planner
+} // namespace aikido
