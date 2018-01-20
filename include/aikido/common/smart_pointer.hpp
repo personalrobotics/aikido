ff1c2ccc4bc93d89442a6e332ecc966d332d7c14
Jeongseok Lee <jslee02@gmail.com>
2018-01-19 16:16:29 -0800

    Add smart_pointer.hpp

---
+++ include/aikido/common/smart_pointer.hpp
@@ -0,0 +1,21 @@
+#ifndef AIKIDO_COMMON_SMARTPOINTER_HPP_
+#define AIKIDO_COMMON_SMARTPOINTER_HPP_
+
+#include <dart/common/SmartPointer.hpp>
+
+// clang-format off
+#define AIKIDO_COMMON_DECLARE_SMART_POINTERS(X)                                \
+  DART_COMMON_MAKE_SHARED_WEAK(X)                                              \
+  using Unique ## X ## Ptr      = std::unique_ptr< X >;                        \
+  using UniqueConst ## X ## Ptr = std::unique_ptr< const X >;
+// clang-format on
+
+namespace aikido {
+namespace common {
+
+// Declare here the smart pointers of aikido::common objects
+
+} // namespace common
+} // namespace aikido
+
+#endif // AIKIDO_COMMON_SMARTPOINTER_HPP_
---
+++ include/aikido/trajectory/smart_pointer.hpp
@@ -0,0 +1,15 @@
+#ifndef AIKIDO_TRAJECTORY_SMARTPOINTER_HPP_
+#define AIKIDO_TRAJECTORY_SMARTPOINTER_HPP_
+
+#include "aikido/common/smart_pointer.hpp"
+
+namespace aikido {
+namespace trajectory {
+
+AIKIDO_COMMON_DECLARE_SMART_POINTERS(Spline)
+AIKIDO_COMMON_DECLARE_SMART_POINTERS(Interpolated)
+
+} // namespace trajectory
+} // namespace aikido
+
+#endif // AIKIDO_TRAJECTORY_SMARTPOINTER_HPP_
