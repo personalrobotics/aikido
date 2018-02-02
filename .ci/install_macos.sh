feb9a0b003d644bd2c0a15390ad53491fd57a017
Jeongseok Lee <jslee02@gmail.com>
2018-02-02 12:03:50 -0800

    Fix typo

--- .ci/install_macos.sh
+++ .ci/install_macos.sh
@@ -4,7 +4,7 @@ set -e
 
 brew update > /dev/null
 
-brew install dartsim/dart/dartsim6 --without-optimizer-ipopt --without-gui --without-optimizer-planning
+brew install dartsim/dart/dartsim6 --without-optimizer-ipopt --without-gui --without-planning
 brew install ompl --with-eigen
 brew install tinyxml2
 brew install yaml-cpp
