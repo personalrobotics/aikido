e03d04a6821dbb2ead4fe970fa538fd64feeb5a8
Brian Hou <brianhou@users.noreply.github.com>
2018-02-10 21:55:14 -0800

    Configure Travis CI to automatically deploy docs from master. (#332)
    * Configure Travis CI to automatically deploy docs from master.
    
    * Add list of API versions to splash page.
    
    * Set DOWNLOAD_TAGFILES to include DART tags.

---
+++ .ci/build_docs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Build documentation
+./scripts/internal-run.sh catkin build --no-status --no-deps -p 1 -i --make-args docs -- aikido > /dev/null
+
+# Organize into "master" subdirectory
+mkdir -p "${TRAVIS_BUILD_DIR}/gh-pages"
+mv "${HOME}/workspace/build/aikido/doxygen" "${TRAVIS_BUILD_DIR}/gh-pages/master"
+
+# Generate hard-coded list of API versions
+cat <<EOF > "${TRAVIS_BUILD_DIR}/gh-pages/README.md"
+## AIKIDO API Documentation
+
+* [master](https://personalrobotics.github.io/aikido/master/)
+EOF
