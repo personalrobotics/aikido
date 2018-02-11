17dab7b38cf9a837f13a7c2a348a7f254c70566b
Brian Hou <brianhou@users.noreply.github.com>
2018-02-10 17:02:11 -0800

    Configure Travis CI to automatically deploy docs from master (#331)
    * Add Travis CI configuration for automatically deploying docs from master.
    
    * Install Doxygen.
    
    * Clean up scripts, assuming Doxygen is pre-installed by pr-cleanroom.

---
+++ .ci/build_docs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Build documentation
+./scripts/internal-run.sh catkin build --no-status --no-deps -p 1 -i --make-args docs -- aikido > /dev/null
+
+# Organize into "master" subdirectory
+mkdir -p "${HOME}/gh-pages"
+mv "${HOME}/workspace/build/aikido/doxygen" "${HOME}/gh-pages/master"
