2a2716d9fdb838a7fde7358aa3e49f66c88f8532
Brian Hou <brianmhou@gmail.com>
2018-02-10 16:29:59 -0800

    Install Doxygen.

--- .ci/before_install_linux.sh
+++ .ci/before_install_linux.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Install documentation dependencies
+sudo -n apt-get -qqy install --no-install-recommends doxygen
+
 # Install test fixture dependencies.
 mkdir -p "${HOME}/workspace/src"
 cd "${HOME}/workspace"
