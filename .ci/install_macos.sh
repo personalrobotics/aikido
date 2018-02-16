f2d013a56ecb3ccd4dc5cd53b8d1fa7dc55b95da
Jeongseok Lee <jslee02@gmail.com>
2018-02-16 07:36:25 -0800

    Add more dependencies available on Homebrew

--- .ci/install_macos.sh
+++ .ci/install_macos.sh
@@ -4,7 +4,9 @@ set -e
 
 brew update > /dev/null
 
+brew install boost
 brew install dartsim
+brew install libmicrohttpd
 brew install ompl --with-eigen
 brew install tinyxml2
 brew install yaml-cpp
