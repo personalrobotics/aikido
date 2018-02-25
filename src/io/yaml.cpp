860f43109c9ea6b1930947fa3f56ef3897dd8ce8
Gilwoo Lee <gilwoo301@gmail.com>
2018-02-24 17:12:38 -0800

    Address PR

--- src/io/yaml.cpp
+++ src/io/yaml.cpp
@@ -7,19 +7,7 @@ YAML::Node loadYAML(
     const dart::common::Uri& yamlUri,
     const dart::common::ResourceRetrieverPtr& retriever)
 {
-  // These can be replaced with retriever::readAll(uri) with Dart 6.2.
-  // std::string content = retriever->readAll(yamlUri);
-  const auto resource = retriever->retrieve(yamlUri);
-  const auto resourceSize = resource->getSize();
-  std::string content;
-  content.resize(resourceSize);
-  if (resource->read(&content.front(), resourceSize, 1) != 1)
-  {
-    std::stringstream message;
-    message << "Unable to load from '" << yamlUri.getPath() << "'.";
-    throw std::runtime_error(message.str());
-  }
-
+  std::string content = retriever->readAll(yamlUri);
   return YAML::Load(content);
 }
 
