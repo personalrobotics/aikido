55a7be58736ec82a07b09bc708e64f47662c8cb8
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-03-09 14:07:19 -0800

    Update install_linux.sh

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+if [ ${TRAVIS} = "true" ]; then
+  AIKIDO_BUILD_DIR="${TRAVIS_BUILD_DIR}"
+elif [ ${APPVEYOR} = "True" ]; then
+  AIKIDO_BUILD_DIR="${APPVEYOR_BUILD_FOLDER}"
+fi
+
 cp -r "${AIKIDO_BUILD_DIR}" src
 ./scripts/internal-distro.py --workspace=src distribution.yml --repository "${REPOSITORY}"
