5fb45b60231b14fef7929e192424a7ec9b900009
Jeongseok Lee <jslee02@gmail.com>
2018-03-19 16:27:02 -0700

    Set CTEST_OUTPUT_ON_FAILURE=true right before make test

--- .ci/script_linux.sh
+++ .ci/script_linux.sh
@@ -12,7 +12,7 @@ set -e
 ./scripts/internal-run.sh catkin build --no-status --no-deps -p 1 -i --cmake-args -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DTREAT_WARNINGS_AS_ERRORS=ON -DCODECOV=$CODECOV --make-args tests -- aikido
 
 # Run tests and measure test coverage if CodeCov is on.
-if [ $CODECOV = ON ]; then ./scripts/internal-run.sh make -C build/aikido aikido_coverage; else ./scripts/internal-run.sh make -C build/aikido test; fi
+if [ $CODECOV = ON ]; then ./scripts/internal-run.sh CTEST_OUTPUT_ON_FAILURE=true make -C build/aikido aikido_coverage; else ./scripts/internal-run.sh make -C build/aikido test; fi
 
 # Uploading report to CodeCov
 if [ $CODECOV = ON ]; then bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"; fi
