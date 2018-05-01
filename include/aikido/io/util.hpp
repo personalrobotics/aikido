07249d77f5f88e639a528d9e10477a03ddf05ef6
Daniel Gallenberger <danielgallenberger@online.de>
2018-04-30 17:56:22 -0700

    integrated PR feedback about loadSkeletonFromUrdf

--- include/aikido/io/util.hpp
+++ include/aikido/io/util.hpp
@@ -1,18 +1,15 @@
 #ifndef AIKIDO_IO_UTIL_HPP_
 #define AIKIDO_IO_UTIL_HPP_
 
-#include <aikido/io/CatkinResourceRetriever.hpp>
 #include <dart/dart.hpp>
 #include <dart/utils/urdf/DartLoader.hpp>
-
-using dart::dynamics::SkeletonPtr;
+#include "aikido/io/CatkinResourceRetriever.hpp"
 
 namespace aikido {
 namespace io {
 
-const SkeletonPtr makeBodyFromURDF(
-    const std::shared_ptr<aikido::io::CatkinResourceRetriever>
-        resourceRetriever,
+dart::dynamics::SkeletonPtr loadSkeletonFromURDF(
+    const dart::common::ResourceRetrieverPtr resourceRetriever,
     const std::string& uri,
     const Eigen::Isometry3d& transform);
 
