9cb1bbef2593162207660fa990d69f8e8f061bcd
Daniel Gallenberger <danielgallenberger@online.de>
2018-04-30 17:16:00 -0700

    added check for empty MetaSkeletons

--- src/planner/vectorfield/VectorFieldPlanner.cpp
+++ src/planner/vectorfield/VectorFieldPlanner.cpp
@@ -141,8 +141,11 @@ std::unique_ptr<aikido::trajectory::Spline> planToEndEffectorOffset(
     std::chrono::duration<double> timelimit,
     planner::PlanningResult* planningResult)
 {
-
   // ensure that no two planners run at the same time
+  if (metaskeleton->getNumBodyNodes() <= 0)
+  {
+      throw std::runtime_error("MetaSkeleton doesn't have any body nodes.");
+  }
   auto robot = metaskeleton->getBodyNode(0)->getSkeleton();
   std::lock_guard<std::mutex> lock(robot->getMutex());
 
