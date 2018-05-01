50bce40b29b6e3027a7c85a945f7e986a1a52c3e
Daniel Gallenberger <danielgallenberger@online.de>
2018-05-01 12:26:40 -0700

    formatting

--- include/aikido/io.hpp
+++ include/aikido/io.hpp
@@ -1,4 +1,4 @@
 #include "io/CatkinResourceRetriever.hpp"
 #include "io/KinBodyParser.hpp"
-#include "io/yaml.hpp"
 #include "io/util.hpp"
+#include "io/yaml.hpp"
--- src/io/util.cpp
+++ src/io/util.cpp
@@ -9,19 +9,24 @@ dart::dynamics::SkeletonPtr loadSkeletonFromURDF(
     const Eigen::Isometry3d& transform = Eigen::Isometry3d::Identity())
 {
   dart::utils::DartLoader urdfLoader;
-  const dart::dynamics::SkeletonPtr skeleton = urdfLoader.parseSkeleton(uri, resourceRetriever);
+  const dart::dynamics::SkeletonPtr skeleton
+      = urdfLoader.parseSkeleton(uri, resourceRetriever);
 
   if (!skeleton)
     throw std::runtime_error("unable to load '" + uri + "'");
 
-  if (skeleton->getNumJoints() <= 0) {
+  if (skeleton->getNumJoints() <= 0)
+  {
     throw std::runtime_error("Skeleton is empty.");
   }
 
-  auto freeJoint = dynamic_cast<dart::dynamics::FreeJoint*>(skeleton->getRootJoint(0));
+  auto freeJoint
+      = dynamic_cast<dart::dynamics::FreeJoint*>(skeleton->getRootJoint(0));
 
-  if (freeJoint == nullptr) {
-      throw std::runtime_error("unable to cast Skeleton's root joint to FreeJoint.");
+  if (freeJoint == nullptr)
+  {
+    throw std::runtime_error(
+        "unable to cast Skeleton's root joint to FreeJoint.");
   }
   freeJoint->setTransform(transform);
 
