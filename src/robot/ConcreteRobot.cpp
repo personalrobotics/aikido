841021f61a3fdd703e25e9434e79fb327f8a0eaf
Jeongseok Lee <jslee02@gmail.com>
2018-05-06 10:52:30 -0700

    Format code

--- src/robot/ConcreteRobot.cpp
+++ src/robot/ConcreteRobot.cpp
@@ -29,9 +29,9 @@ using dart::dynamics::MetaSkeletonPtr;
 // These should be defined when we construct planner adapter classes
 // TODO(JS): timelimit, maxNumTrials, and collisionResolution are disabled since
 // not used.
-//static const double timelimit = 3.0;
-//static const std::size_t maxNumTrials = 10;
-//static const double collisionResolution = 0.1;
+// static const double timelimit = 3.0;
+// static const std::size_t maxNumTrials = 10;
+// static const double collisionResolution = 0.1;
 static const double asymmetryTolerance = 1e-3;
 
 namespace {
