1562ff6da0bf6ae295f75bf284eb35b93b5b8647
Aditya Vamsikrishna <adityavk711@gmail.com>
2018-05-06 18:19:25 -0700

    add more tests

--- tests/constraint/test_SequentialSampleable.cpp
+++ tests/constraint/test_SequentialSampleable.cpp
@@ -11,6 +11,7 @@
 
 using aikido::statespace::R1;
 using aikido::statespace::R2;
+using aikido::constraint::CyclicSampleable;
 using aikido::constraint::FiniteSampleable;
 using aikido::constraint::ConstSampleablePtr;
 using aikido::constraint::SequentialSampleable;
@@ -22,6 +23,7 @@ EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Vector2d)
 TEST(SequentialSampleableTest, ConstructorThrowsOnNullStateSpace)
 {
   auto r1 = std::make_shared<R1>();
+
   auto s1 = r1->createState();
   s1.setValue(aikido::tests::make_vector(-2));
 
@@ -35,6 +37,7 @@ TEST(SequentialSampleableTest, ConstructorThrowsOnNullStateSpace)
 TEST(SequentialSampleableTest, ConstructorThrowsOnEmptySampleables)
 {
   auto r1 = std::make_shared<R1>();
+
   auto s1 = r1->createState();
   s1.setValue(aikido::tests::make_vector(-2));
 
@@ -46,20 +49,21 @@ TEST(SequentialSampleableTest, ConstructorThrowsOnEmptySampleables)
 TEST(SequentialSampleableTest, ConstructorThrowsOnNullSampleable)
 {
   auto r1 = std::make_shared<R1>();
+
   auto s1 = r1->createState();
   s1.setValue(aikido::tests::make_vector(-2));
 
   std::vector<ConstSampleablePtr> sampleables;
   sampleables.emplace_back(nullptr);
 
-  EXPECT_THROW(
-      SequentialSampleable(nullptr, sampleables), std::invalid_argument);
+  EXPECT_THROW(SequentialSampleable(r1, sampleables), std::invalid_argument);
 }
 
 TEST(SequentialSampleableTest, ConstructorThrowsOnStateSpaceMismatch)
 {
   auto r1 = std::make_shared<R1>();
   auto r2 = std::make_shared<R2>();
+
   auto s1 = r1->createState();
   s1.setValue(aikido::tests::make_vector(-2));
 
@@ -71,12 +75,69 @@ TEST(SequentialSampleableTest, ConstructorThrowsOnStateSpaceMismatch)
 
 TEST(SequentialSampleableTest, SingleSampleGenerator)
 {
-  // Test generator characterstics
-  EXPECT_TRUE(true);
+  auto r1 = std::make_shared<R1>();
+
+  auto s1 = r1->createState();
+  Eigen::VectorXd v = aikido::tests::make_vector(-2);
+  s1.setValue(v);
+
+  std::vector<ConstSampleablePtr> sampleables;
+  sampleables.emplace_back(std::make_shared<const FiniteSampleable>(r1, s1));
+
+  SequentialSampleable constraint(r1, sampleables);
+
+  // Single-generator.
+  std::unique_ptr<SampleGenerator> generator
+      = constraint.createSampleGenerator();
+
+  auto state = r1->createState();
+
+  // Test the generator's behavior.
+  EXPECT_TRUE(generator->canSample());
+  EXPECT_EQ(generator->getNumSamples(), 1);
+
+  generator->sample(state);
+  EXPECT_TRUE(state.getValue().isApprox(v));
+
+  EXPECT_FALSE(generator->canSample());
+  EXPECT_EQ(generator->getNumSamples(), 0);
 }
 
 TEST(SequentialSampleableTest, MultipleSampleGenerators)
 {
-  // Test generator characterstics
-  EXPECT_TRUE(true);
+  auto r1 = std::make_shared<R1>();
+
+  auto s1 = r1->createState();
+  auto s2 = r1->createState();
+  Eigen::VectorXd v1 = aikido::tests::make_vector(-2);
+  Eigen::VectorXd v2 = aikido::tests::make_vector(-1);
+  s1.setValue(v1);
+  s2.setValue(v2);
+
+  std::vector<ConstSampleablePtr> sampleables;
+  sampleables.emplace_back(std::make_shared<const FiniteSampleable>(r1, s1));
+  sampleables.emplace_back(
+        std::make_shared<const CyclicSampleable>(
+          std::make_shared<FiniteSampleable>(r1, s2)));
+
+  SequentialSampleable constraint(r1, sampleables);
+
+  // Multiple-generator.
+  std::unique_ptr<SampleGenerator> generator
+      = constraint.createSampleGenerator();
+
+  auto state = r1->createState();
+
+  // Test the generator's behavior.
+  EXPECT_TRUE(generator->canSample());
+  EXPECT_EQ(generator->getNumSamples(), SampleGenerator::NO_LIMIT);
+
+  generator->sample(state);
+  EXPECT_TRUE(state.getValue().isApprox(v1));
+
+  EXPECT_TRUE(generator->canSample());
+  EXPECT_EQ(generator->getNumSamples(), SampleGenerator::NO_LIMIT);
+
+  generator->sample(state);
+  EXPECT_TRUE(state.getValue().isApprox(v2));
 }
