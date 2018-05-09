965269bb335a173d3c1637265db125645f86d571
Jeongseok Lee <jslee02@gmail.com>
2018-05-09 12:53:01 -0700

    Remove AppVeyor condition

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -3,12 +3,5 @@
 set -ex
 
 cd "${HOME}/workspace"
-
-if [ ${TRAVIS} ]; then
-  AIKIDO_BUILD_DIR="${TRAVIS_BUILD_DIR}"
-elif [ ${APPVEYOR} ]; then
-  AIKIDO_BUILD_DIR="${APPVEYOR_BUILD_FOLDER}"
-fi
-
-cp -r "${AIKIDO_BUILD_DIR}" src
+cp -r "${TRAVIS_BUILD_DIR}" src
 ./scripts/internal-distro.py --workspace=src distribution.yml --repository "${REPOSITORY}" ${REQUIRED_ONLY}
