e6ad991ad0baa0106905ceb3eda38220f306515e
Brian Hou <brian.hou@live.com>
2018-05-09 14:21:14 -0700

    Introduce a separate DOCS entry in the build matrix.
    Build documentation for a list of different versions.

--- .ci/build_docs.sh
+++ .ci/build_docs.sh
@@ -4,16 +4,32 @@ set -ex
 
 cd "${HOME}/workspace"
 
-# Build documentation
-./scripts/internal-run.sh catkin build --no-status --no-deps -p 1 -i --cmake-args -DDOWNLOAD_TAGFILES=ON --make-args docs -- aikido > /dev/null
+AIKIDO_DIR="${HOME}/workspace/src/aikido"
+BUILD_DIR="${HOME}/workspace/build/aikido/doxygen"
 
-# Organize into "master" subdirectory
-mkdir -p "${TRAVIS_BUILD_DIR}/gh-pages"
-mv "${HOME}/workspace/build/aikido/doxygen" "${TRAVIS_BUILD_DIR}/gh-pages/master"
+# For branch builds, Travis only clones that branch with a fixed depth of 50
+# commits. This means that the clone knows nothing about other Git branches or
+# tags. We fix this by deleting and re-cloning the full repository.
+rm -rf ${AIKIDO_DIR}
+git clone "https://github.com/${TRAVIS_REPO_SLUG}.git" ${AIKIDO_DIR}
 
-# Generate hard-coded list of API versions
-cat <<EOF > "${TRAVIS_BUILD_DIR}/gh-pages/README.md"
+# Organize into "gh-pages" directory
+mkdir -p ${HOME}/gh-pages
+
+# Initialize list of API versions
+cat <<EOF > ${HOME}/gh-pages/README.md
 ## API Documentation
 
-* [master](https://personalrobotics.github.io/aikido/master/)
 EOF
+
+while read version; do
+  # Add entry to list of API versions
+  echo "* [${version}](https://personalrobotics.github.io/aikido/${version}/)" >> ${HOME}/gh-pages/README.md
+
+  # Build documentation
+  git -C ${AIKIDO_DIR} checkout ${version}
+  rm -rf ${BUILD_DIR}
+  ./scripts/internal-run.sh catkin build --no-status --no-deps -p 1 -i --cmake-args -DDOWNLOAD_TAGFILES=ON --make-args docs -- aikido > /dev/null
+
+  mv ${BUILD_DIR} ${HOME}/gh-pages/${version}
+done < ${TRAVIS_BUILD_DIR}/.ci/docs_versions.txt
--- .ci/script_linux.sh
+++ .ci/script_linux.sh
@@ -4,6 +4,13 @@ set -ex
 
 cd "${HOME}/workspace"
 
+if [ $BUILD_NAME = DOCS ]; then
+  # Set up Catkin workspace without building Aikido
+  ./scripts/internal-build.sh
+  . "${TRAVIS_BUILD_DIR}/.ci/build_docs.sh"
+  exit 0
+fi
+
 ./scripts/internal-build.sh ${PACKAGE_NAMES}
 ./scripts/internal-test.sh ${PACKAGE_NAMES}
 
