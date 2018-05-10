960f1e0051b8994814e97957303376238c9be97f
Brian Hou <brianhou@users.noreply.github.com>
2018-05-09 22:31:25 -0700

    Define variables where they're used. (#407)

--- .ci/before_install_linux.sh
+++ .ci/before_install_linux.sh
@@ -8,4 +8,3 @@ cd "${HOME}/workspace"
 git clone https://github.com/personalrobotics/pr-cleanroom.git scripts
 curl -sS "${DISTRIBUTION}" > distribution.yml
 ./scripts/internal-setup.sh
-export PACKAGE_NAMES="$(./scripts/internal-get-packages.py distribution.yml ${REPOSITORY})"
