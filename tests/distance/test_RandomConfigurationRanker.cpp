99aaa49f1dcc2d3abc7ef247d1ce2251eb02d471
Aditya Vamsikrishna <adityavk711@gmail.com>
2018-05-22 14:11:35 -0700

    clean up test scripts

--- tests/distance/test_RandomConfigurationRanker.cpp
+++ tests/distance/test_RandomConfigurationRanker.cpp
@@ -30,16 +30,16 @@ protected:
   void SetUp() override
   {
     // Manipulator with 2 revolute joints.
-    mManipulator1 = Skeleton::create("Manipulator1");
+    mManipulator = Skeleton::create("Manipulator");
 
     // Root joint
     RevoluteJoint::Properties properties1;
     properties1.mAxis = Eigen::Vector3d::UnitY();
     properties1.mName = "Joint1";
 
-    bn1 = mManipulator1
+    bn1 = mManipulator
               ->createJointAndBodyNodePair<RevoluteJoint>(
-                  nullptr, properties1, create_BodyNodeProperties("root_body"))
+                  nullptr, properties1, create_BodyNodeProperties("root"))
               .second;
 
     // joint 2, body 2
@@ -47,54 +47,53 @@ protected:
     properties2.mAxis = Eigen::Vector3d::UnitY();
     properties2.mName = "Joint2";
     properties2.mT_ParentBodyToJoint.translation() = Eigen::Vector3d(0, 0, 1);
-    bn2 = mManipulator1
+    bn2 = mManipulator
               ->createJointAndBodyNodePair<RevoluteJoint>(
-                  bn1, properties2, create_BodyNodeProperties("second_body"))
+                  bn1, properties2, create_BodyNodeProperties("leaf"))
               .second;
 
-    mStateSpace1
-        = std::make_shared<MetaSkeletonStateSpace>(mManipulator1.get());
+    mStateSpace = std::make_shared<MetaSkeletonStateSpace>(mManipulator.get());
 
     Eigen::Vector2d defaultPosition(0.0, 0.0);
-    mManipulator1->setPositions(defaultPosition);
+    mManipulator->setPositions(defaultPosition);
   }
 
-  SkeletonPtr mManipulator1;
-  MetaSkeletonStateSpacePtr mStateSpace1;
+  SkeletonPtr mManipulator;
+  MetaSkeletonStateSpacePtr mStateSpace;
   BodyNodePtr bn1, bn2;
 };
 
 TEST_F(RandomConfigurationRankerTest, Constructor)
 {
   auto seedStateOne
-      = mStateSpace1->getScopedStateFromMetaSkeleton(mManipulator1.get());
+      = mStateSpace->getScopedStateFromMetaSkeleton(mManipulator.get());
   seedStateOne.getSubStateHandle<SO2>(0).fromAngle(0.1);
   seedStateOne.getSubStateHandle<SO2>(1).fromAngle(0.1);
   std::vector<aikido::statespace::StateSpace::State*> states;
   states.emplace_back(seedStateOne);
 
   EXPECT_THROW(
-      RandomConfigurationRanker(nullptr, mManipulator1, states),
+      RandomConfigurationRanker(nullptr, mManipulator, states),
       std::invalid_argument);
 
   EXPECT_THROW(
-      RandomConfigurationRanker(mStateSpace1, nullptr, states),
+      RandomConfigurationRanker(mStateSpace, nullptr, states),
       std::invalid_argument);
 
   EXPECT_THROW(
-      RandomConfigurationRanker(mStateSpace1, nullptr, states),
+      RandomConfigurationRanker(mStateSpace, nullptr, states),
       std::invalid_argument);
 
-  RandomConfigurationRanker ranker(mStateSpace1, mManipulator1, states);
+  RandomConfigurationRanker ranker(mStateSpace, mManipulator, states);
   DART_UNUSED(ranker);
 }
 
 TEST_F(RandomConfigurationRankerTest, OrderTest)
 {
   auto seedStateOne
-      = mStateSpace1->getScopedStateFromMetaSkeleton(mManipulator1.get());
+      = mStateSpace->getScopedStateFromMetaSkeleton(mManipulator.get());
   auto seedStateTwo
-      = mStateSpace1->getScopedStateFromMetaSkeleton(mManipulator1.get());
+      = mStateSpace->getScopedStateFromMetaSkeleton(mManipulator.get());
   seedStateOne.getSubStateHandle<SO2>(0).fromAngle(0.1);
   seedStateOne.getSubStateHandle<SO2>(1).fromAngle(0.1);
   seedStateTwo.getSubStateHandle<SO2>(0).fromAngle(0.2);
@@ -104,12 +103,12 @@ TEST_F(RandomConfigurationRankerTest, OrderTest)
   states.emplace_back(seedStateOne);
   states.emplace_back(seedStateTwo);
 
-  RandomConfigurationRanker ranker(mStateSpace1, mManipulator1, states);
+  RandomConfigurationRanker ranker(mStateSpace, mManipulator, states);
 
   auto rankedSolutions = ranker.getRankedIKSolutions();
 
-  auto rankedStateOne = mStateSpace1->cloneState(rankedSolutions[0].first);
-  auto rankedStateTwo = mStateSpace1->cloneState(rankedSolutions[1].first);
+  auto rankedStateOne = mStateSpace->cloneState(rankedSolutions[0].first);
+  auto rankedStateTwo = mStateSpace->cloneState(rankedSolutions[1].first);
 
   ASSERT_NEAR(rankedStateOne.getSubStateHandle<SO2>(0).toAngle(), 0.1, 1e-5);
   ASSERT_NEAR(rankedStateOne.getSubStateHandle<SO2>(1).toAngle(), 0.1, 1e-5);
