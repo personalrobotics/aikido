47aa57b10c5a13887e68c2bb7a09a9311b6b0aa4
Aditya Vamsikrishna <adityavk711@gmail.com>
2018-05-23 18:48:38 -0700

    make format

--- src/distance/JointAvoidanceConfigurationRanker.cpp
+++ src/distance/JointAvoidanceConfigurationRanker.cpp
@@ -31,7 +31,7 @@ double JointAvoidanceConfigurationRanker::evaluateConfiguration(
 {
   Eigen::VectorXd solutionPosition(mMetaSkeletonStateSpace->getDimension());
   mMetaSkeletonStateSpace->convertStateToPositions(
-        mMetaSkeletonStateSpace->cloneState(solution), solutionPosition);
+      mMetaSkeletonStateSpace->cloneState(solution), solutionPosition);
 
   auto lowerLimits = mMetaSkeleton->getPositionLowerLimits();
   auto upperLimits = mMetaSkeleton->getPositionUpperLimits();
@@ -42,11 +42,14 @@ double JointAvoidanceConfigurationRanker::evaluateConfiguration(
   for (auto index : mUnboundedUpperLimitsIndices)
     upperLimits[index] = solutionPosition[index];
 
-  auto lowerLimitsState = mMetaSkeletonStateSpace->getStateFromPositions(lowerLimits);
-  auto upperLimitsState = mMetaSkeletonStateSpace->getStateFromPositions(upperLimits);
+  auto lowerLimitsState
+      = mMetaSkeletonStateSpace->getStateFromPositions(lowerLimits);
+  auto upperLimitsState
+      = mMetaSkeletonStateSpace->getStateFromPositions(upperLimits);
 
-  return -std::min(mDistanceMetric->distance(solution, lowerLimitsState),
-                   mDistanceMetric->distance(solution, upperLimitsState));
+  return -std::min(
+      mDistanceMetric->distance(solution, lowerLimitsState),
+      mDistanceMetric->distance(solution, upperLimitsState));
 }
 
 } // namespace distance
