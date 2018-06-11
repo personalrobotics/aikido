0af34053afcfec1b91a6a79531c053303e943297
Daqing Yi <dqyi@ava.personalrobotics.ri.cmu.edu>
2018-06-11 10:32:27 -0700

    add docstrings and tests for post-processing

--- include/aikido/planner/kinodynamic/KinodynamicTimer.hpp
+++ include/aikido/planner/kinodynamic/KinodynamicTimer.hpp
@@ -12,23 +12,23 @@ namespace kinodynamic {
 
 /// Computes the time-optimal timing of a trajectory consisting of a sequence
 /// Geodesic interpolations between states under velocity and acceleration
-/// bounds. The output is a parabolic spline, encoded in cubic polynomials,
-/// that \b exactly follows the input path.
+/// bounds. The output is a parabolic spline, encoded in cubic polynomials.
+/// It firstly preprocesses a non-differentiable path to a differentiable one
+/// by adding circular blends; and then \b exactly follows the preprocessed
+/// path.
 ///
 /// The output trajectory consists of a sequence of trapezoidal velocity
-/// profiles that implement bang-bang control. This trajectory must stop at
-/// each waypoint that introduces a velocity discontinuity to satisfy finite
-/// acceleration bounds. You should consider using a blending or smoothing
-/// algorithm, which does \b not follow the exact input path, if this behavior
-/// is undesirable.
+/// profiles that implement bang-bang control. This function curently only
+/// supports \c RealVector, \c SO2, and compound state spaces of those types.
+/// Additionally, this function requires that \c inputTrajectory to be
+/// interpolated using a \c GeodesicInterpolator.
 ///
-/// This function curently only supports \c RealVector, \c SO2, and compound
-/// state spaces of those types. Additionally, this function requires that
-/// \c _inputTrajectory to be interpolated using a \c GeodesicInterpolator.
-///
-/// \param _inputTrajectory input piecewise Geodesic trajectory
-/// \param _maxVelocity maximum velocity for each dimension
-/// \param _maxAcceleration maximum acceleration for each dimension
+/// \param[in] inputTrajectory input piecewise Geodesic trajectory
+/// \param[in] maxVelocity maximum velocity for each dimension
+/// \param[in] maxAcceleration maximum acceleration for each dimension
+/// \param[in] maxDeviation maximum deviation from a waypoint in doing circular
+/// blending around the waypoint
+/// \param[in] timeStep time step in following the path
 /// \return time optimal trajectory that satisfies acceleration constraints
 std::unique_ptr<aikido::trajectory::Spline> computeKinodynamicTiming(
     const aikido::trajectory::Interpolated& inputTrajectory,
@@ -37,6 +37,27 @@ std::unique_ptr<aikido::trajectory::Spline> computeKinodynamicTiming(
     double maxDeviation = 1e-2,
     double timeStep = 0.1);
 
+/// Computes the time-optimal timing of a spline trajectory under velocity
+/// and acceleration bounds. The output is another parabolic spline, encoded
+/// in cubic polynomials. In retiming, the input trajectory is used as a
+/// piecewise Geodesic trajectory (only the geometry of the input trajectory
+/// is considered; and the velocity information is ignored. It firstly
+/// preprocesses a non-differentiable path to a differentiable one by adding
+/// circular blends; and then \b exactly follows the preprocessed path.
+///
+/// The output trajectory consists of a sequence of trapezoidal velocity
+/// profiles that implement bang-bang control. This function curently only
+/// supports \c RealVector, \c SO2, and compound state spaces of those types.
+/// Additionally, this function requires that \c inputTrajectory to be
+/// a spline (only the geometry of the trajectory is considered in retiming).
+///
+/// \param[in] inputTrajectory input piecewise Geodesic trajectory
+/// \param[in] maxVelocity maximum velocity for each dimension
+/// \param[in] maxAcceleration maximum acceleration for each dimension
+/// \param[in] maxDeviation maximum deviation from a waypoint in doing circular
+/// blending around the waypoint
+/// \param[in] timeStep time step in following the path
+/// \return time optimal trajectory that satisfies acceleration constraints
 std::unique_ptr<aikido::trajectory::Spline> computeKinodynamicTiming(
     const aikido::trajectory::Spline& inputTrajectory,
     const Eigen::VectorXd& maxVelocity,
@@ -50,8 +71,10 @@ std::unique_ptr<aikido::trajectory::Spline> computeKinodynamicTiming(
 class KinodynamicTimer : public aikido::planner::TrajectoryPostProcessor
 {
 public:
-  /// \param _velocityLimits Maximum velocity for each dimension.
-  /// \param _accelerationLimits Maximum acceleration for each dimension.
+  /// \param[in] velocityLimits Maximum velocity for each dimension.
+  /// \param[in] accelerationLimits Maximum acceleration for each dimension.
+  /// \param[in] maxDeviation Maximum deviation in circular blending
+  /// \param[in] timeStep Time step in following the path
   KinodynamicTimer(
       const Eigen::VectorXd& velocityLimits,
       const Eigen::VectorXd& accelerationLimits,
@@ -72,31 +95,41 @@ public:
       const aikido::common::RNG& rng,
       const aikido::constraint::TestablePtr& constraint = nullptr) override;
 
+  /// Returns the velocity limits of the dimensions
   const Eigen::VectorXd& getVelocityLimits() const;
-   
+
+  /// Returns the acceleration limits of the dimensions
   const Eigen::VectorXd& getAccelerationLimits() const;
 
+  /// Sets the velocity limits of the dimensions
   void setVelocityLimits(const Eigen::VectorXd& velocityLimits);
   
+  /// Sets the acceleration limits of the dimensions
   void setAccelerationLimits(const Eigen::VectorXd& accelerationLimits);
 
+  /// Returns the time step in following a path
   double getTimeStep() const;
 
+  /// Sets the time step in following a path
   void setTimeStep(double timeStep);
 
+  /// Returns the max deviation of circular blending
   double getMaxDeviation() const;
 
+  /// Sets the max deviation of circular blending
   void setMaxDeviation(double maxDeviation);
 
 private:
-  /// Set to the value of \c _velocityLimits.
+  /// Set to the value of \c velocityLimits.
   Eigen::VectorXd mVelocityLimits;
 
-  /// Set to the value of \c _accelerationLimits.
+  /// Set to the value of \c accelerationLimits.
   Eigen::VectorXd mAccelerationLimits;
 
+  /// Set to the value of \c maxDeviation
   double mMaxDeviation;
 
+  /// Set to the value of \c timeStep
   double mTimeStep;
 };
 
---
+++ tests/planner/kinodynamic/test_KinodynamicTimerPostProcessor.cpp
@@ -0,0 +1,129 @@
+#include <gtest/gtest.h>
+#include <aikido/common/RNG.hpp>
+#include <aikido/planner/kinodynamic/KinodynamicTimer.hpp>
+#include <aikido/planner/parabolic/ParabolicTimer.hpp>
+#include <aikido/statespace/GeodesicInterpolator.hpp>
+#include <aikido/statespace/Rn.hpp>
+#include "eigen_tests.hpp"
+
+using Eigen::Vector2d;
+using aikido::trajectory::Interpolated;
+using aikido::statespace::GeodesicInterpolator;
+using aikido::statespace::R2;
+using aikido::planner::parabolic::convertToSpline;
+using aikido::planner::kinodynamic::KinodynamicTimer;
+
+class KinodynamicTimerPostProcessorTests : public ::testing::Test
+{
+protected:
+  void SetUp() override
+  {
+    mRng = aikido::common::RNGWrapper<std::mt19937>(0);
+    mStateSpace = std::make_shared<R2>();
+    mInterpolator = std::make_shared<GeodesicInterpolator>(mStateSpace);
+
+    mMaxDeviation = 1e-1;
+    mTimeStep = 0.01;
+  }
+
+  aikido::common::RNGWrapper<std::mt19937> mRng;
+  std::shared_ptr<R2> mStateSpace;
+  std::shared_ptr<GeodesicInterpolator> mInterpolator;
+
+  double mMaxDeviation;
+  double mTimeStep;
+};
+
+TEST_F(KinodynamicTimerPostProcessorTests, testTime)
+{
+  KinodynamicTimer testKinodynamicTimerPostProcessor(
+      Vector2d::Constant(2.), Vector2d::Constant(1.),
+      mMaxDeviation, mTimeStep);
+
+  Interpolated inputTrajectory(mStateSpace, mInterpolator);
+
+  auto state = mStateSpace->createState();
+  Eigen::VectorXd tangentVector;
+
+  // The optimal timing of this trajectory should be a triangle centered at t =
+  // 1that accelerates at 1 rad/s^2 for 1 s, then deaccelerates at -1 rad/s^2
+  // for 1 s. This corresponds to moving each axis through 2 rad.
+  state.setValue(Vector2d(1., 2.));
+  inputTrajectory.addWaypoint(0., state);
+
+  state.setValue(Vector2d(2., 3.));
+  inputTrajectory.addWaypoint(2., state);
+
+  auto timedTrajectory
+      = testKinodynamicTimerPostProcessor.postprocess(inputTrajectory, mRng);
+
+  // TODO: Why does this return three derivatives instead of two?
+  EXPECT_GE(timedTrajectory->getNumDerivatives(), 2);
+  EXPECT_EQ(2, timedTrajectory->getNumSegments());
+  EXPECT_DOUBLE_EQ(2., timedTrajectory->getDuration());
+
+  // Position.
+  timedTrajectory->evaluate(0., state);
+  EXPECT_TRUE(Vector2d(1.0, 2.0).isApprox(state.getValue()));
+
+  timedTrajectory->evaluate(1., state);
+  EXPECT_TRUE(Vector2d(1.5, 2.5).isApprox(state.getValue()));
+
+  timedTrajectory->evaluate(2., state);
+  EXPECT_TRUE(Vector2d(2.0, 3.0).isApprox(state.getValue()));
+
+  // Velocity
+  timedTrajectory->evaluateDerivative(0.5, 1, tangentVector);
+  EXPECT_TRUE(Vector2d(0.5, 0.5).isApprox(tangentVector));
+
+  timedTrajectory->evaluateDerivative(1.0, 1, tangentVector);
+  EXPECT_TRUE(Vector2d(1.0, 1.0).isApprox(tangentVector));
+
+  timedTrajectory->evaluateDerivative(1.5, 1, tangentVector);
+  EXPECT_TRUE(Vector2d(0.5, 0.5).isApprox(tangentVector));
+
+  // Acceleration.
+  timedTrajectory->evaluateDerivative(0.5, 2, tangentVector);
+  EXPECT_TRUE(Vector2d(1., 1.).isApprox(tangentVector));
+
+  timedTrajectory->evaluateDerivative(1.5, 2, tangentVector);
+  EXPECT_TRUE(Vector2d(-1., -1.).isApprox(tangentVector));
+}
+
+TEST_F(KinodynamicTimerPostProcessorTests, testSplineTiming)
+{
+  KinodynamicTimer testKinodynamicTimerPostProcessor(
+      Vector2d::Constant(2.), Vector2d::Constant(1.),
+      mMaxDeviation, mTimeStep);
+
+  Interpolated interpolated(mStateSpace, mInterpolator);
+
+  auto state = mStateSpace->createState();
+  auto state2 = mStateSpace->createState();
+
+  // This is the same test as StraightLine_TriangularProfile, except that the
+  // trajectory starts at a non-zero time.
+  state.setValue(Vector2d(1., 2.));
+  interpolated.addWaypoint(1., state);
+
+  state.setValue(Vector2d(2., 3.));
+  interpolated.addWaypoint(3., state);
+
+  auto spline = convertToSpline(interpolated);
+
+  auto timedInterpolated
+      = testKinodynamicTimerPostProcessor.postprocess(interpolated, mRng);
+  auto timedSpline = testKinodynamicTimerPostProcessor.postprocess(*spline, mRng);
+
+  timedInterpolated->evaluate(1., state);
+  timedSpline->evaluate(1., state2);
+  EXPECT_TRUE(state2.getValue().isApprox(state.getValue()));
+
+  timedInterpolated->evaluate(2., state);
+  timedSpline->evaluate(2., state2);
+  EXPECT_TRUE(state2.getValue().isApprox(state.getValue()));
+
+  timedInterpolated->evaluate(3., state);
+  timedSpline->evaluate(3., state2);
+  EXPECT_TRUE(state2.getValue().isApprox(state.getValue()));
+}
