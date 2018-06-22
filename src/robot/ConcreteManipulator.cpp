d2dfa3eaa8c357b1d17df04d0cf0846bb0291831
Sherdil Niyaz <sniyaz@berkeley.edu>
2018-06-22 08:15:32 -0400

    Move getEndEffectorDirection into robot/util

--- src/robot/ConcreteManipulator.cpp
+++ src/robot/ConcreteManipulator.cpp
@@ -139,14 +139,6 @@ trajectory::TrajectoryPtr ConcreteManipulator::planToEndEffectorOffset(
 }
 
 //==============================================================================
-Eigen::Vector3d ConcreteManipulator::getEndEffectorDirection(
-    const dart::dynamics::BodyNodePtr& body) const
-{
-  const std::size_t zDirection = 2;
-  return body->getWorldTransform().linear().col(zDirection).normalized();
-}
-
-//==============================================================================
 trajectory::TrajectoryPtr ConcreteManipulator::planEndEffectorStraight(
     statespace::dart::MetaSkeletonStateSpacePtr& space,
     const dart::dynamics::MetaSkeletonPtr& metaSkeleton,
@@ -160,7 +152,7 @@ trajectory::TrajectoryPtr ConcreteManipulator::planEndEffectorStraight(
   auto collision
       = getFullCollisionConstraint(space, metaSkeleton, collisionFree);
 
-  Eigen::Vector3d direction = getEndEffectorDirection(body);
+  Eigen::Vector3d direction = util::getEndEffectorDirection(body);
 
   if (distance < 0)
   {
