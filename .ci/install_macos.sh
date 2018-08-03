fbb9ac28afcdc460c683318f9efe034413e778c1
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-08-02 21:33:37 -0700

    Update install_macos.sh

--- .ci/install_macos.sh
+++ .ci/install_macos.sh
@@ -4,5 +4,9 @@ set -ex
 
 /usr/bin/yes | pip2 uninstall numpy  # see https://github.com/travis-ci/travis-ci/issues/6688
 
-brew upgrade > /dev/null
+# sudo npm uninstall npm -g
+# sudo rm -rf /usr/local/lib/node_modules/npm
+# brew link node
+
+# brew upgrade > /dev/null
 brew bundle || true
