560aa977d22ee08f13ccf453df1536846ea8b46e
Jeongseok Lee <jslee02@users.noreply.github.com>
2018-11-30 15:45:16 -0800

    Update script_linux.sh

--- .ci/script_linux.sh
+++ .ci/script_linux.sh
@@ -6,8 +6,9 @@ cd "${HOME}/workspace"
 
 export PACKAGE_NAMES="$(./scripts/internal-get-packages.py distribution.yml ${REPOSITORY})"
 if [ $(lsb_release -sc) = "xenial" ]; then
-  . /opt/ros/kinetic/setup.bash
-  ./scripts/internal-build.sh ${PACKAGE_NAMES} --cmake-args -DCMAKE_PREFIX_PATH=/usr
+  #. /opt/ros/kinetic/setup.bash
+  #./scripts/internal-build.sh ${PACKAGE_NAMES} --cmake-args -DCMAKE_PREFIX_PATH=/usr
+  ./scripts/internal-build.sh ${PACKAGE_NAMES}
 else
   ./scripts/internal-build.sh ${PACKAGE_NAMES}
 fi
