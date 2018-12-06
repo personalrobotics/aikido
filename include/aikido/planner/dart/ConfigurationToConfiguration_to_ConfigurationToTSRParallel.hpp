d1eee402c965c95e82649141da2193f9b3c8eda5
Gilwoo Lee <gilwoo301@gmail.com>
2018-12-05 17:49:53 -0800

    Add ConfToConf_ConfToTSRParallel

---
+++ include/aikido/planner/dart/ConfigurationToConfiguration_to_ConfigurationToTSRParallel.hpp
@@ -0,0 +1,39 @@
+#ifndef AIKIDO_PLANNER_DART_CONFIGURATIONTOCONFIGURATIONTOCONFIGURATIONTOTSRPARALLEL_HPP_
+#define AIKIDO_PLANNER_DART_CONFIGURATIONTOCONFIGURATIONTOCONFIGURATIONTOTSRPARALLEL_HPP_
+
+#include "aikido/planner/ConfigurationToConfigurationPlanner.hpp"
+#include "aikido/planner/dart/ConfigurationToConfiguration_to_ConfigurationToTSR.hpp"
+#include "aikido/planner/dart/ConcreteParallelMetaPlanner.hpp"
+
+namespace aikido {
+namespace planner {
+namespace dart {
+
+/// Converts a non-DART ConfigurationToConfiguration planner into a DART
+/// ConfigurationToTSR planner.
+class ConfigurationToConfiguration_to_ConfigurationToTSRParallel
+    : public ConcreteParallelMetaPlanner
+{
+public:
+  /// Constructor
+  ///
+  /// \param[in] stateSpace StateSpace in which the planner operates.
+  /// \param[in] metaSkeleton MetaSkeleton for adapted planner to operate on.
+  /// \param[in] planner Planner to parallelize
+  /// TSR.
+  ConfigurationToConfiguration_to_ConfigurationToTSRParallel(
+      statespace::dart::ConstMetaSkeletonStateSpacePtr stateSpace,
+      ::dart::dynamics::MetaSkeletonPtr metaSkeleton,
+      const std::shared_ptr<ConfigurationToConfiguration_to_ConfigurationToTSR>&
+          planner,
+      std::size_t numCopies = 1,
+      const std::vector<common::RNG*> rngs =
+        std::vector<common::RNG*>());
+
+};
+
+} // namespace dart
+} // namespace planner
+} // namespace aikido
+
+#endif // AIKIDO_PLANNER_DART_CONFIGURATIONTOCONFIGURATIONTOCONFIGURATIONTOTSR_HPP_
