0905824d4f1a8de46138b4f023fba3587304a4e2
Gilwoo Lee <gilwoo301@gmail.com>
2018-12-09 15:29:11 -0800

    Detach stopped planner threads

--- src/planner/dart/ConcreteParallelMetaPlanner.cpp
+++ src/planner/dart/ConcreteParallelMetaPlanner.cpp
@@ -175,8 +175,7 @@ trajectory::TrajectoryPtr ConcreteParallelMetaPlanner::plan(
             planner->stopPlanning();
 
           for(auto& thread: threads)
-            if (thread.joinable())
-              thread.join();
+            thread.detach();
 
           // Return
           {
