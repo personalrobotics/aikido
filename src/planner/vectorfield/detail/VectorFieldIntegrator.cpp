6228a8e8c2c58b91645c04a3a217506530d7c771
Jeongseok Lee <jslee02@users.noreply.github.com>
2019-01-16 17:09:33 -0800

    Fix compiling dart::common::make_unique() with recent GCC versions (#481)

--- src/planner/vectorfield/detail/VectorFieldIntegrator.cpp
+++ src/planner/vectorfield/detail/VectorFieldIntegrator.cpp
@@ -16,11 +16,10 @@ std::unique_ptr<aikido::trajectory::Spline> convertToSpline(
     const std::vector<Knot>& knots,
     aikido::statespace::ConstStateSpacePtr stateSpace)
 {
-  using dart::common::make_unique;
-
   std::size_t dimension = stateSpace->getDimension();
 
-  auto outputTrajectory = make_unique<aikido::trajectory::Spline>(stateSpace);
+  auto outputTrajectory
+      = ::dart::common::make_unique<aikido::trajectory::Spline>(stateSpace);
 
   using CubicSplineProblem = aikido::common::
       SplineProblem<double, int, 2, Eigen::Dynamic, Eigen::Dynamic>;
