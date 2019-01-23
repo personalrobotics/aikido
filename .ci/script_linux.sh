acc71fda8f8bc7a77e0f2fd997e2b0f013f67802
Brian Hou <brianmhou@gmail.com>
2019-01-23 13:30:08 -0800

    Specify target file to upload to Codecov.

--- .ci/script_linux.sh
+++ .ci/script_linux.sh
@@ -33,5 +33,5 @@ fi
 
 # Uploading report to CodeCov
 if [ $BUILD_NAME = TRUSTY_FULL_DEBUG ]; then
-  bash <(curl -s https://codecov.io/bash) -X gcov || echo "Codecov did not collect coverage reports"
+  bash <(curl -s https://codecov.io/bash) -X gcov -f "${HOME}/workspace/build/aikido/coverage.info.cleaned" || echo "Codecov did not collect coverage reports"
 fi
