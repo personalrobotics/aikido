48bb65e0df7574a6cecf7a252cbe057cc6d40fa4
Gilwoo Lee <gilwoo301@gmail.com>
2019-04-17 16:12:18 -0700

    Address PR comments by Brian

--- src/rviz/shape_conversions.cpp
+++ src/rviz/shape_conversions.cpp
@@ -193,7 +193,7 @@ bool convertShape(
   marker->scale = convertEigenToROSVector3(shape.getScale());
 
   const aiScene* scene = shape.getMesh();
-  const std::string& meshUri = shape.getMeshPath();
+  const std::string& meshPath = shape.getMeshPath();
   if (!meshUri.empty())
   {
     marker->type = Marker::MESH_RESOURCE;
