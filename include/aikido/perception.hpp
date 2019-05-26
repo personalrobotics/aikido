39db697531a528efce81f291420f67ec7245f501
Jeongseok Lee <jslee02@gmail.com>
2019-05-25 19:39:26 -0700

    Disable finding octomap_ros and VoxelGridModule

--- include/aikido/perception.hpp
+++ include/aikido/perception.hpp
@@ -2,5 +2,5 @@
 #include "perception/DetectedObject.hpp"
 #include "perception/PerceptionModule.hpp"
 #include "perception/PoseEstimatorModule.hpp"
-#include "perception/VoxelGridModule.hpp"
+// #include "perception/VoxelGridModule.hpp"
 #include "perception/shape_conversions.hpp"
