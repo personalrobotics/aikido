f08abe785bcf08a316112e51c94edbaac9fd067f
Gilwoo Lee <gilwoo301@gmail.com>
2020-02-25 13:39:50 -0800

    Remove planToConfiguration, planToConfigurations, planToTSR, pltnToTSRwithTrajectoryConstraint, planToEndEffectorOffset, planToEndEffectorOffsetByCRRT from robot::util

--- include/aikido/robot/util.hpp
+++ include/aikido/robot/util.hpp
@@ -93,62 +93,6 @@ struct CRRTPlannerParameters
   double projectionTolerance;
 };
 
-/// Plan the robot to a specific configuration.
-/// Restores the robot to its initial configuration after planning.
-/// \param[in] space The StateSpace for the metaskeleton
-/// \param[in] metaSkeleton MetaSkeleton to plan with.
-/// \param[in] goalState Goal state
-/// \param[in] collisionTestable Testable constraint to check for collision.
-/// \param[in] rng Random number generator
-/// \param[in] timelimit Max time to spend per planning to each IK
-trajectory::TrajectoryPtr planToConfiguration(
-    const statespace::dart::MetaSkeletonStateSpacePtr& space,
-    const dart::dynamics::MetaSkeletonPtr& metaSkeleton,
-    const statespace::StateSpace::State* goalState,
-    const constraint::TestablePtr& collisionTestable,
-    common::RNG* rng,
-    double timelimit);
-
-/// Plan the robot to a set of configurations.
-/// Restores the robot to its initial configuration after planning.
-/// \param[in] space The StateSpace for the metaskeleton
-/// \param[in] metaSkeleton MetaSkeleton to plan with.
-/// \param[in] goalStates Goal states
-/// \param[in] collisionTestable Testable constraint to check for collision.
-/// \param[in] rng Random number generator
-/// \param[in] timelimit Max time to spend per planning to each IK
-trajectory::TrajectoryPtr planToConfigurations(
-    const statespace::dart::MetaSkeletonStateSpacePtr& space,
-    const dart::dynamics::MetaSkeletonPtr& metaSkeleton,
-    const std::vector<statespace::StateSpace::State*>& goalStates,
-    const constraint::TestablePtr& collisionTestable,
-    common::RNG* rng,
-    double timelimit);
-
-/// Plan the configuration of the metakeleton such that
-/// the specified bodynode is set to a sample in TSR
-/// \param[in] space The StateSpace for the metaskeleton.
-/// \param[in] metaSkeleton MetaSkeleton to plan with.
-/// \param[in] bodyNode Bodynode whose frame for which TSR is constructed.
-/// \param[in] tsr TSR to plan to.
-/// \param[in] collisionTestable Testable constraint to check for collision.
-/// \param[in] rng Random number generator
-/// \param[in] timelimit Max time (seconds) to spend per planning to each IK
-/// \param[in] maxNumTrials Number of retries before failure.
-/// \param[in] ranker Ranker to rank the sampled configurations. If nullptr,
-/// NominalConfigurationRanker is used with the current metaSkeleton pose.
-/// \return Trajectory to a sample in TSR, or nullptr if planning fails.
-trajectory::TrajectoryPtr planToTSR(
-    const statespace::dart::MetaSkeletonStateSpacePtr& space,
-    const dart::dynamics::MetaSkeletonPtr& metaSkeleton,
-    const dart::dynamics::BodyNodePtr& bodyNode,
-    const constraint::dart::TSRPtr& tsr,
-    const constraint::TestablePtr& collisionTestable,
-    common::RNG* rng,
-    double timelimit,
-    std::size_t maxNumTrials,
-    const distance::ConstConfigurationRankerPtr& ranker = nullptr);
-
 /// Returns a Trajectory that moves the configuration of the metakeleton such
 /// that the specified bodynode is set to a sample in a goal TSR and
 /// the trajectory is constrained to a constraint TSR
@@ -170,59 +114,6 @@ trajectory::InterpolatedPtr planToTSRwithTrajectoryConstraint(
     const constraint::dart::TSRPtr& constraintTsr,
     const constraint::TestablePtr& collisionTestable,
     double timelimit,
-    const CRRTPlannerParameters& crrtParameters = CRRTPlannerParameters());
-
-/// Plan to a desired end-effector offset with fixed orientation.
-/// \param[in] space StateSpace for the metaskeleton
-/// \param[in] metaSkeleton Metaskeleton to plan with
-/// \param[in] body Bodynode for the end-effector
-/// \param[in] direction Direction unit vector in the world frame
-/// \param[in] collisionTestable Collision constraint to check. Self-collision
-/// is checked by default.
-/// \param[in] distance Distance distance to move, in meters
-/// \param[in] timelimit Timelimit for planning
-/// \param[in] positionTolerance Tolerance in position
-/// \param[in] angularTolerance Tolerance in angle
-/// \param[in] vfParameters VectorFieldPlanenr parameters
-/// \param[in] crrtParameters CRRTPlanner parameters
-/// \return Output trajectory
-trajectory::TrajectoryPtr planToEndEffectorOffset(
-    const statespace::dart::MetaSkeletonStateSpacePtr& space,
-    const dart::dynamics::MetaSkeletonPtr& metaSkeleton,
-    const dart::dynamics::BodyNodePtr& body,
-    const Eigen::Vector3d& direction,
-    const constraint::TestablePtr& collisionTestable,
-    double distance,
-    double timelimit,
-    double positionTolerance = 1e-3,
-    double angularTolerance = 1e-3,
-    const VectorFieldPlannerParameters& vfParameters
-    = VectorFieldPlannerParameters(),
-    const CRRTPlannerParameters& crrtParameters = CRRTPlannerParameters());
-
-/// Plan to a desired end-effector offset with fixed orientation using CRRT.
-/// \param[in] space StateSpace for the metaskeleton
-/// \param[in] metaSkeleton Metaskeleton to plan with
-/// \param[in] bodyNode Bodynode for the end-effector
-/// \param[in] direction Direction unit vector in the world frame
-/// \param[in] collisionTestable Collision constraint to check. Self-collision
-/// is checked by default.
-/// \param[in] distance Distance distance to move, in meters
-/// \param[in] timelimit Timelimit for planning
-/// \param[in] positionTolerance Tolerance in position
-/// \param[in] angularTolerance Tolerance in angle
-/// \param[in] crrtParameters CRRTPlanner parameters
-/// \return Output trajectory
-trajectory::InterpolatedPtr planToEndEffectorOffsetByCRRT(
-    const statespace::dart::MetaSkeletonStateSpacePtr& space,
-    const dart::dynamics::MetaSkeletonPtr& metaSkeleton,
-    const dart::dynamics::BodyNodePtr& bodyNode,
-    const constraint::TestablePtr& collisionTestable,
-    const Eigen::Vector3d& direction,
-    double distance,
-    double timelimit,
-    double positionTolerance = 1e-3,
-    double angularTolerance = 1e-3,
     const CRRTPlannerParameters& crrtParameters = CRRTPlannerParameters());
 
 /// Parses YAML node for named configurtaions
