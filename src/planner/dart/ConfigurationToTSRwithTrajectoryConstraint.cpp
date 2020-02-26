e86e8e728dd4d9579c56fa7d752697cabcea3e39
Gilwoo Lee <gilwoo301@gmail.com>
2020-02-25 19:21:34 -0800

    Add ConfigurationToTSRwithTrajectoryConstraint, CRRTConfigurationToTSRwithTrajectoryConstraintPlanner

---
+++ src/planner/dart/ConfigurationToTSRwithTrajectoryConstraint.cpp
@@ -0,0 +1,55 @@
+#include "aikido/planner/dart/ConfigurationToTSRwithTrajectoryConstraint.hpp"
+
+#include "aikido/constraint/Testable.hpp"
+
+namespace aikido {
+namespace planner {
+namespace dart {
+
+//==============================================================================
+ConfigurationToTSRwithTrajectoryConstraint::ConfigurationToTSRwithTrajectoryConstraint(
+    statespace::dart::ConstMetaSkeletonStateSpacePtr stateSpace,
+    ::dart::dynamics::ConstMetaSkeletonPtr metaSkeleton,
+    ::dart::dynamics::ConstBodyNodePtr endEffectorBodyNode,
+    constraint::dart::ConstTSRPtr goalTSR,
+    constraint::dart::ConstTSRPtr constraintTSR,
+    constraint::ConstTestablePtr constraint)
+  : ConfigurationToTSR(
+    std::move(stateSpace),
+    std::move(metaSkeleton),
+    std::move(endEffectorBodyNode),
+    0, // This parameter is not used in this class.
+    std::move(goalTSR),
+    std::move(constraint))
+  , mConstraintTSR(constraintTSR)
+{
+  // Do nothing.
+}
+
+//==============================================================================
+ConfigurationToTSRwithTrajectoryConstraint::ConfigurationToTSRwithTrajectoryConstraint(
+    statespace::dart::ConstMetaSkeletonStateSpacePtr stateSpace,
+    const statespace::dart::MetaSkeletonStateSpace::State* startState,
+    ::dart::dynamics::ConstBodyNodePtr endEffectorBodyNode,
+    constraint::dart::ConstTSRPtr goalTSR,
+    constraint::dart::ConstTSRPtr constraintTSR,
+    constraint::ConstTestablePtr constraint)
+  : ConfigurationToTSR(
+    std::move(stateSpace), startState,
+    std::move(endEffectorBodyNode),
+    0, // This parameter is not used in this class.
+    std::move(goalTSR), std::move(constraint))
+  , mConstraintTSR(constraintTSR)
+{
+  // Do nothing.
+}
+
+//==============================================================================
+constraint::dart::ConstTSRPtr ConfigurationToTSRwithTrajectoryConstraint::getConstraintTSR() const
+{
+  return mConstraintTSR;
+}
+
+} // namespace dart
+} // namespace planner
+} // namespace aikido
