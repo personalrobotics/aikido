a7d0171b0364c9db1663197a75edcb937a1f874c
Aditya Vamsikrishna Mandalika <adityavk@cs.uw.edu>
2020-09-16 21:35:25 -0700

    move implementations up to base class

--- include/aikido/robot/Robot.hpp
+++ include/aikido/robot/Robot.hpp
@@ -2,15 +2,22 @@
 #define AIKIDO_ROBOT_ROBOT_HPP_
 
 #include <chrono>
+#include <string>
 #include <unordered_map>
 
 #include <Eigen/Core>
 #include <dart/dynamics/dynamics.hpp>
 
+#include "aikido/common/ExecutorThread.hpp"
 #include "aikido/common/RNG.hpp"
 #include "aikido/constraint/dart/CollisionFree.hpp"
 #include "aikido/constraint/dart/TSR.hpp"
 #include "aikido/control/TrajectoryExecutor.hpp"
+#include "aikido/distance/ConfigurationRanker.hpp"
+#include "aikido/planner/parabolic/ParabolicSmoother.hpp"
+#include "aikido/planner/parabolic/ParabolicTimer.hpp"
+#include "aikido/robot/Robot.hpp"
+#include "aikido/robot/util.hpp"
 #include "aikido/statespace/dart/MetaSkeletonStateSpace.hpp"
 #include "aikido/trajectory/Spline.hpp"
 #include "aikido/trajectory/Trajectory.hpp"
@@ -21,62 +28,83 @@ namespace robot {
 AIKIDO_DECLARE_POINTERS(Robot)
 
 /// Robot interface for defining basic behaviors of a robot.
-/// A concrete implementation of robot should support a collection
-/// of planning methods.
 class Robot
 {
 public:
+  /// Constructor.
+  /// \param[in] name Name of the robot.
+  /// \param[in] metaSkeleton Metaskeleton of the robot.
+  /// \param[in] rng Random number generator.
+  /// \param[in] trajectoryExecutor Trajectory executor for the metaSkeleton.
+  /// \param[in] collisionDetector Collision detector.
+  /// \param[in] selfCollisionFilter Collision filter for self collision.
+  // TODO(avk): Why should trajectory executor be passed in? Contruct in place?
+  // TODO(avk): Why should collision detector be passed in? Construct in place?
+  // TODO(avk): Why should self collision filter be passed in? Get from SRDF?
+  // TODO(avk): Why should rng be passed in? What is it even used for?
+  // TODO(avk): Why should metaskeleton be passed in? Construct from
+  Robot(
+      const std::string& name,
+      dart::dynamics::MetaSkeletonPtr metaSkeleton,
+      aikido::common::UniqueRNGPtr rng,
+      aikido::control::TrajectoryExecutorPtr trajectoryExecutor,
+      dart::collision::CollisionDetectorPtr collisionDetector,
+      std::shared_ptr<dart::collision::BodyNodeCollisionFilter>
+          selfCollisionFilter);
+
+  /// Destructor.
   virtual ~Robot() = default;
 
-  /// Executes a trajectory
-  /// \param[in] trajectory Timed trajectory to execute
-  virtual std::future<void> executeTrajectory(
-      const trajectory::TrajectoryPtr& trajectory) const = 0;
-
-  /// Returns a named configuration
-  /// \param[in] name Name of the configuration
-  /// \return Configuration
-  virtual boost::optional<Eigen::VectorXd> getNamedConfiguration(
-      const std::string& name) const = 0;
-
-  /// Sets the list of named configurations
-  /// \param[in] namedConfigurations Map of name, configuration
-  virtual void setNamedConfigurations(
-      std::unordered_map<std::string, const Eigen::VectorXd>
-          namedConfigurations)
-      = 0;
-
-  /// \return Name of this Robot
-  virtual std::string getName() const = 0;
+  /// \return Name of this robot.
+  virtual std::string getName() const;
 
   /// \return [const] MetaSkeleton of this robot.
-  virtual dart::dynamics::ConstMetaSkeletonPtr getMetaSkeleton() const = 0;
+  virtual dart::dynamics::ConstMetaSkeletonPtr getMetaSkeleton() const;
 
   /// \return MetaSkeleton of this robot.
   dart::dynamics::MetaSkeletonPtr getMetaSkeleton();
 
   /// \return [const] MetaSkeletonStateSpace of this robot.
   virtual aikido::statespace::dart::ConstMetaSkeletonStateSpacePtr
-  getStateSpace() const = 0;
+  getStateSpace() const;
 
   /// \return MetaSkeletonStateSpace of this robot.
   aikido::statespace::dart::MetaSkeletonStateSpacePtr getStateSpace();
 
-  /// Sets the root of this robot.
-  /// \param[in] robot Parent robot
-  virtual void setRoot(Robot* robot) = 0;
+  /// Returns a named configuration.
+  /// \param[in] name Name of the configuration.
+  /// \return Configuration.
+  virtual boost::optional<Eigen::VectorXd> getNamedConfiguration(
+      const std::string& name) const;
+
+  /// Sets the list of named configurations.
+  /// \param[in] namedConfigurations Map of name, configuration.
+  virtual void setNamedConfigurations(
+      std::unordered_map<std::string, const Eigen::VectorXd>
+          namedConfigurations);
+
+  /// Sets the root of this robot. If this robot instance is a part of another
+  /// robot object, the root allows computation of the full collision
+  /// constraint.
+  /// \param[in] robot Root robot.
+  virtual void setRoot(Robot* robot);
+
+  /// Executes a trajectory.
+  /// \param[in] trajectory Timed trajectory to execute.
+  virtual std::future<void> executeTrajectory(
+      const trajectory::TrajectoryPtr& trajectory) const;
 
   /// Simulates up to the provided timepoint.
   /// \note Assumes that the robot's skeleton is locked.
   /// \param[in] timepoint Time to simulate to.
-  virtual void step(const std::chrono::system_clock::time_point& timepoint) = 0;
+  virtual void step(const std::chrono::system_clock::time_point& timepoint);
 
   /// Returns self-collision constraint.
   /// \param[in] space Space in which this collision constraint operates.
   /// \param[in] metaSkeleton Metaskeleton for the statespace.
   virtual constraint::dart::CollisionFreePtr getSelfCollisionConstraint(
       const statespace::dart::ConstMetaSkeletonStateSpacePtr& space,
-      const dart::dynamics::MetaSkeletonPtr& metaSkeleton) const = 0;
+      const dart::dynamics::MetaSkeletonPtr& metaSkeleton) const;
 
   /// TODO: Consider changing this to return a CollisionFreePtr.
   /// Combines provided collision constraint with self collision
@@ -88,7 +116,62 @@ public:
   virtual constraint::TestablePtr getFullCollisionConstraint(
       const statespace::dart::ConstMetaSkeletonStateSpacePtr& space,
       const dart::dynamics::MetaSkeletonPtr& metaSkeleton,
-      const constraint::dart::CollisionFreePtr& collisionFree) const = 0;
+      const constraint::dart::CollisionFreePtr& collisionFree) const;
+
+  /// Computes velocity limits from the MetaSkeleton. These should be
+  /// interpreted as absolute in both the positive and negative directions.
+  /// \param[in] metaSkeleton MetaSkeleton to compute limits for.
+  /// \return Symmetric velocity limits.
+  Eigen::VectorXd getVelocityLimits(
+      const dart::dynamics::MetaSkeleton& metaSkeleton) const;
+
+  /// Computes acceleration limits from the MetaSkeleton. These should be
+  /// interpreted as absolute in both the positive and negative directions.
+  /// \param[in] metaSkeleton MetaSkeleton to compute limits for.
+  /// \return Symmetric acceleration limits.
+  Eigen::VectorXd getAccelerationLimits(
+      const dart::dynamics::MetaSkeleton& metaSkeleton) const;
+
+private:
+  // Named Configurations are read from a YAML file
+  using ConfigurationMap
+      = std::unordered_map<std::string, const Eigen::VectorXd>;
+
+  /// Clones the associated random number generator.
+  /// \return a cloned copy of the associated RNG.
+  std::unique_ptr<aikido::common::RNG> cloneRNG();
+
+  /// If this robot belongs to another (Composite)Robot,
+  /// mRootRobot is the topmost robot containing this robot.
+  Robot* mRootRobot;
+
+  /// Name of this robot.
+  std::string mName;
+
+  /// MetaSkeleton of this robot.
+  dart::dynamics::MetaSkeletonPtr mMetaSkeleton;
+
+  /// The statespace of this robot.
+  statespace::dart::MetaSkeletonStateSpacePtr mStateSpace;
+
+  /// Skeleton containing mRobot.
+  dart::dynamics::SkeletonPtr mParentSkeleton;
+
+  /// RNG.
+  std::unique_ptr<common::RNG> mRng;
+
+  /// Trajectory executor.
+  std::shared_ptr<control::TrajectoryExecutor> mTrajectoryExecutor;
+
+  /// Collision detector to evaluate collisions.
+  ::dart::collision::CollisionDetectorPtr mCollisionDetector;
+
+  /// Collision filter to ignore collisions between adjacent bodies.
+  std::shared_ptr<dart::collision::BodyNodeCollisionFilter>
+      mSelfCollisionFilter;
+
+  /// Commonly used configurations that the robot can be in.
+  ConfigurationMap mNamedConfigurations;
 };
 
 } // namespace robot
--- src/robot/Robot.cpp
+++ src/robot/Robot.cpp
@@ -1,7 +1,133 @@
 #include "aikido/robot/Robot.hpp"
 
+#include "aikido/constraint/TestableIntersection.hpp"
+#include "aikido/planner/kunzretimer/KunzRetimer.hpp"
+#include "aikido/statespace/StateSpace.hpp"
+
 namespace aikido {
 namespace robot {
+
+using constraint::ConstTestablePtr;
+using constraint::TestablePtr;
+using constraint::dart::CollisionFreePtr;
+using constraint::dart::TSRPtr;
+using planner::TrajectoryPostProcessor;
+using planner::kunzretimer::KunzRetimer;
+using planner::parabolic::ParabolicSmoother;
+using planner::parabolic::ParabolicTimer;
+using statespace::StateSpace;
+using statespace::StateSpacePtr;
+using statespace::dart::ConstMetaSkeletonStateSpacePtr;
+using statespace::dart::MetaSkeletonStateSpace;
+using statespace::dart::MetaSkeletonStateSpacePtr;
+using trajectory::Interpolated;
+using trajectory::InterpolatedPtr;
+using trajectory::Spline;
+using trajectory::TrajectoryPtr;
+using trajectory::UniqueSplinePtr;
+
+using dart::dynamics::BodyNodePtr;
+using dart::dynamics::MetaSkeleton;
+using dart::dynamics::MetaSkeletonPtr;
+
+// TODO: Temporary constants for planning calls.
+// These should be defined when we construct planner adapter classes
+// static const double collisionResolution = 0.1;
+static constexpr double kAsymmetryTolerance = 1e-3;
+
+namespace {
+
+// TODO: These may not generalize to many robots.
+Eigen::VectorXd getSymmetricLimits(
+    const MetaSkeleton& metaSkeleton,
+    const Eigen::VectorXd& lowerLimits,
+    const Eigen::VectorXd& upperLimits,
+    const std::string& limitName,
+    double kAsymmetryTolerance)
+{
+  const auto numDofs = metaSkeleton.getNumDofs();
+  assert(static_cast<std::size_t>(lowerLimits.size()) == numDofs);
+  assert(static_cast<std::size_t>(upperLimits.size()) == numDofs);
+
+  Eigen::VectorXd symmetricLimits(numDofs);
+  for (std::size_t iDof = 0; iDof < numDofs; ++iDof)
+  {
+    symmetricLimits[iDof] = std::min(-lowerLimits[iDof], upperLimits[iDof]);
+    if (std::abs(lowerLimits[iDof] + upperLimits[iDof]) > kAsymmetryTolerance)
+    {
+      dtwarn << "MetaSkeleton '" << metaSkeleton.getName()
+             << "' has asymmetric " << limitName << " limits ["
+             << lowerLimits[iDof] << ", " << upperLimits[iDof]
+             << "] for DegreeOfFreedom '"
+             << metaSkeleton.getDof(iDof)->getName() << "' (index: " << iDof
+             << "). Using a conservative limit of" << symmetricLimits[iDof]
+             << ".";
+    }
+  }
+  return symmetricLimits;
+}
+
+Eigen::VectorXd getSymmetricVelocityLimits(
+    const MetaSkeleton& metaSkeleton, double kAsymmetryTolerance)
+{
+  return getSymmetricLimits(
+      metaSkeleton,
+      metaSkeleton.getVelocityLowerLimits(),
+      metaSkeleton.getVelocityUpperLimits(),
+      "velocity",
+      kAsymmetryTolerance);
+}
+
+Eigen::VectorXd getSymmetricAccelerationLimits(
+    const MetaSkeleton& metaSkeleton, double kAsymmetryTolerance)
+{
+  return getSymmetricLimits(
+      metaSkeleton,
+      metaSkeleton.getAccelerationLowerLimits(),
+      metaSkeleton.getAccelerationUpperLimits(),
+      "acceleration",
+      kAsymmetryTolerance);
+}
+
+} // namespace
+
+//==============================================================================
+Robot::Robot(
+    const std::string& name,
+    MetaSkeletonPtr metaSkeleton,
+    common::UniqueRNGPtr rng,
+    control::TrajectoryExecutorPtr trajectoryExecutor,
+    dart::collision::CollisionDetectorPtr collisionDetector,
+    std::shared_ptr<dart::collision::BodyNodeCollisionFilter>
+        selfCollisionFilter)
+  : mRootRobot(this)
+  , mName(name)
+  , mMetaSkeleton(metaSkeleton)
+  , mStateSpace(std::make_shared<MetaSkeletonStateSpace>(mMetaSkeleton.get()))
+  , mParentSkeleton(nullptr)
+  , mRng(std::move(rng))
+  , mTrajectoryExecutor(std::move(trajectoryExecutor))
+  , mCollisionDetector(collisionDetector)
+  , mSelfCollisionFilter(selfCollisionFilter)
+{
+  if (!mMetaSkeleton)
+  {
+    throw std::invalid_argument("Robot is nullptr.");
+  }
+  mParentSkeleton = mMetaSkeleton->getBodyNode(0)->getSkeleton();
+}
+
+//==============================================================================
+std::string Robot::getName() const
+{
+  return mName;
+}
+
+//==============================================================================
+dart::dynamics::ConstMetaSkeletonPtr Robot::getMetaSkeleton() const
+{
+  return mMetaSkeleton;
+}
 
 //==============================================================================
 dart::dynamics::MetaSkeletonPtr Robot::getMetaSkeleton()
@@ -11,10 +137,141 @@ dart::dynamics::MetaSkeletonPtr Robot::getMetaSkeleton()
 }
 
 //==============================================================================
+statespace::dart::ConstMetaSkeletonStateSpacePtr Robot::getStateSpace() const
+{
+  return mStateSpace;
+}
+
+//==============================================================================
 statespace::dart::MetaSkeletonStateSpacePtr Robot::getStateSpace()
 {
   return std::const_pointer_cast<statespace::dart::MetaSkeletonStateSpace>(
       const_cast<const Robot*>(this)->getStateSpace());
+}
+
+//==============================================================================
+boost::optional<Eigen::VectorXd> Robot::getNamedConfiguration(
+    const std::string& name) const
+{
+  auto configuration = mNamedConfigurations.find(name);
+  if (configuration == mNamedConfigurations.end())
+  {
+    return boost::none;
+  }
+
+  return configuration->second;
+}
+
+//==============================================================================
+void Robot::setNamedConfigurations(
+    std::unordered_map<std::string, const Eigen::VectorXd> namedConfigurations)
+{
+  mNamedConfigurations = std::move(namedConfigurations);
+}
+
+//=============================================================================
+void Robot::setRoot(Robot* robot)
+{
+  if (robot == nullptr)
+  {
+    throw std::invalid_argument("ConcreteRobot is null.");
+  }
+
+  mRootRobot = robot;
+}
+
+//==============================================================================
+std::future<void> Robot::executeTrajectory(
+    const TrajectoryPtr& trajectory) const
+{
+  return mTrajectoryExecutor->execute(trajectory);
+}
+
+//==============================================================================
+void Robot::step(const std::chrono::system_clock::time_point& timepoint)
+{
+  // Assumes that the parent robot is locked
+  mTrajectoryExecutor->step(timepoint);
+}
+
+// ==============================================================================
+CollisionFreePtr Robot::getSelfCollisionConstraint(
+    const ConstMetaSkeletonStateSpacePtr& space,
+    const MetaSkeletonPtr& metaSkeleton) const
+{
+  using constraint::dart::CollisionFree;
+
+  if (mRootRobot != this)
+  {
+    return mRootRobot->getSelfCollisionConstraint(space, metaSkeleton);
+  }
+
+  mParentSkeleton->enableSelfCollisionCheck();
+  mParentSkeleton->disableAdjacentBodyCheck();
+
+  // TODO: Switch to PRIMITIVE once this is fixed in DART.
+  // mCollisionDetector->setPrimitiveShapeType(FCLCollisionDetector::PRIMITIVE);
+  auto collisionOption
+      = dart::collision::CollisionOption(false, 1, mSelfCollisionFilter);
+  auto collisionFreeConstraint = std::make_shared<CollisionFree>(
+      space, metaSkeleton, mCollisionDetector, collisionOption);
+  collisionFreeConstraint->addSelfCheck(
+      mCollisionDetector->createCollisionGroupAsSharedPtr(mMetaSkeleton.get()));
+  return collisionFreeConstraint;
+}
+
+//=============================================================================
+TestablePtr Robot::getFullCollisionConstraint(
+    const ConstMetaSkeletonStateSpacePtr& space,
+    const MetaSkeletonPtr& metaSkeleton,
+    const CollisionFreePtr& collisionFree) const
+{
+  if (mRootRobot != this)
+  {
+    return mRootRobot->getFullCollisionConstraint(
+        space, metaSkeleton, collisionFree);
+  }
+
+  auto selfCollisionFree = getSelfCollisionConstraint(space, metaSkeleton);
+
+  if (!collisionFree)
+  {
+    return selfCollisionFree;
+  }
+
+  // Make testable constraints for collision check
+  std::vector<ConstTestablePtr> constraints;
+  constraints.reserve(2);
+  constraints.emplace_back(selfCollisionFree);
+  if (collisionFree)
+  {
+    if (collisionFree->getStateSpace() != space)
+    {
+      throw std::runtime_error("CollisionFree has incorrect statespace.");
+    }
+    constraints.emplace_back(collisionFree);
+  }
+
+  return std::make_shared<constraint::TestableIntersection>(space, constraints);
+}
+
+//==============================================================================
+Eigen::VectorXd Robot::getVelocityLimits(const MetaSkeleton& metaSkeleton) const
+{
+  return getSymmetricVelocityLimits(metaSkeleton, kAsymmetryTolerance);
+}
+
+//==============================================================================
+Eigen::VectorXd Robot::getAccelerationLimits(
+    const MetaSkeleton& metaSkeleton) const
+{
+  return getSymmetricAccelerationLimits(metaSkeleton, kAsymmetryTolerance);
+}
+
+//==============================================================================
+std::unique_ptr<common::RNG> Robot::cloneRNG()
+{
+  return mRng->clone();
 }
 
 } // namespace robot
