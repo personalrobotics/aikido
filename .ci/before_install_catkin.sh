8e4fee8bf8ded46bd41d0525a6258621e4cd4694
Ethan Gordon <ekgordon@cs.washington.edu>
2021-04-02 17:31:19 -0700

    [WIP] Specifically target focal support from pr-cleanroom

--- .ci/before_install_catkin.sh
+++ .ci/before_install_catkin.sh
@@ -5,6 +5,6 @@ set -ex
 # Install test fixture dependencies.
 mkdir -p "${HOME}/workspace/src"
 cd "${HOME}/workspace"
-git clone https://github.com/personalrobotics/pr-cleanroom.git scripts
+git clone https://github.com/personalrobotics/pr-cleanroom.git -b focal_support_test scripts
 curl -sS "${DISTRIBUTION}" > distribution.yml
 ./scripts/internal-setup.sh
