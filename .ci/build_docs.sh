1106bcc9e1012e9a784c75e698d3da0e4911d24e
Ethan Gordon <ekgordon@cs.washington.edu>
2021-09-07 14:09:06 -0700

    Clone git repo into separate folder

--- .ci/build_docs.sh
+++ .ci/build_docs.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+AIKIDO_DIR="${HOME}/workspace/src/aikido"
+
+# Github Actions doesn't do a full clone of the repository.
+# We do it ourselves in a separate directory
+# To access all branches and tags.
+mkdir -p ${AIKIDO_DIR}
+git clone "https://github.com/${GITHUB_REPOSITORY}.git" ${AIKIDO_DIR}
+
 # Organize into "gh-pages" directory
 mkdir -p ${GITHUB_WORKSPACE}/gh-pages
 
@@ -19,9 +27,9 @@ while read version; do
   echo "* [${version}](https://personalrobotics.github.io/aikido/${version}/)" >> ${GITHUB_WORKSPACE}/gh-pages/README.md
 
   # Build documentation
-  git -C ${GITHUB_WORKSPACE} checkout ${version}
+  git -C ${AIKIDO_DIR} checkout ${version}
   rm -rf *
-  cmake -DDOWNLOAD_TAGFILES=ON ${GITHUB_WORKSPACE}
+  cmake -DDOWNLOAD_TAGFILES=ON ${AIKIDO_DIR}
   make docs
   mv doxygen ${GITHUB_WORKSPACE}/gh-pages/${version}
 done < ${GITHUB_WORKSPACE}/.ci/docs_versions.txt
