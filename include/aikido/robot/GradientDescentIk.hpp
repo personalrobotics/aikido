19852417f11bd9e08b0135a16a2441b7959178df
sniyaz <sniyaz@berkeley.edu>
2021-12-02 23:29:11 -0800

    Draft getsolutions() in GradientDescentIk!

--- include/aikido/robot/GradientDescentIk.hpp
+++ include/aikido/robot/GradientDescentIk.hpp
@@ -4,7 +4,11 @@
 #include <vector>
 #include <Eigen/Dense>
 
+#include <dart/dart.hpp>
+
+#include "aikido/constraint/Sampleable.hpp"
 #include "aikido/robot/InverseKinematics.hpp"
+#include "aikido/statespace/dart/MetaSkeletonStateSpace.hpp"
 
 namespace aikido {
 namespace robot {
@@ -20,6 +24,14 @@ public:
   // Documentation inherited.
   std::vector<Eigen::VectorXd> getSolutions(
     const Eigen::Isometry3d& targetPose, const size_t maxSolutions) const override;
+  
+private:
+
+  ::dart::dynamics::MetaSkeletonPtr mArm;
+  statespace::dart::ConstMetaSkeletonStateSpacePtr mArmSpace;
+
+  std::shared_ptr<aikido::constraint::Sampleable> mSeedSampleable;
+  ::dart::dynamics::InverseKinematicsPtr mDartIkSolver;
 };
 
 } // namespace robot
--- src/robot/GradientDescentIk.cpp
+++ src/robot/GradientDescentIk.cpp
@@ -1,5 +1,10 @@
 #include "aikido/robot/GradientDescentIk.hpp"
 
+#include "aikido/statespace/dart/MetaSkeletonStateSaver.hpp"
+
+using aikido::constraint::SampleGenerator;
+using aikido::statespace::dart::MetaSkeletonStateSaver;
+
 namespace aikido {
 namespace robot {
 
@@ -14,7 +19,33 @@ std::vector<Eigen::VectorXd> GradientDescentIk::getSolutions(
     const Eigen::Isometry3d& targetPose,
     const size_t maxSolutions
 ) const {
-  // TODO.
+   auto saver = MetaSkeletonStateSaver(
+      mArm, MetaSkeletonStateSaver::Options::POSITIONS);
+  DART_UNUSED(saver);
+
+  std::vector<Eigen::VectorXd> solutions;
+
+  std::shared_ptr<SampleGenerator> ikSeedGenerator
+      = mSeedSampleable->createSampleGenerator();
+  auto seedState = mArmSpace->createState();
+
+  // How many times the IK solver will re-sample a single solution if it
+  // is out of tolerance.
+  for (int i = 0; i < maxSolutions; i++)
+  {
+    // TODO: What to do if we can't sample a seed config?
+    if (!ikSeedGenerator->sample(seedState))
+      continue;
+
+    mArmSpace->setState(mArm.get(), seedState);
+    mDartIkSolver->getTarget()->setTransform(targetPose);
+    bool withinTol = mDartIkSolver->solveAndApply(true);
+
+    if (withinTol)
+      solutions.push_back(mArm->getPositions());
+  }
+
+  return solutions;
 }
 
 } // namespace robot
