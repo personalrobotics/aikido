1e8938ecb68ae5f00f62cc48672e7117525b79aa
sniyaz <sniyaz@berkeley.edu>
2021-12-02 21:18:42 -0800

    Add shell GradientDescentIk source file.

---
+++ src/robot/ik/GradientDescentIk.cpp
@@ -0,0 +1,23 @@
+#include "aikido/robot/ik/GradientDescentIk.hpp"
+
+namespace aikido {
+namespace robot {
+namespace ik {
+
+//==============================================================================
+GradientDescentIk::GradientDescentIk()
+{
+  // TODO.
+}
+
+//==============================================================================
+GradientDescentIk::getSolutions(
+    const Eigen::Isometry3d& targetPose,
+    const size_t maxSolutions
+) {
+  // TODO.
+}
+
+} // namespace ik
+} // namespace robot
+} // namespace aikido
